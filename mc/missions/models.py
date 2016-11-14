a213ebd3db14db734bc1ca275bcbc81abfcc4314
adorsk <adorsk@fas.harvard.edu>
2016-11-14 16:00:05 -0500

    start of missions app

---
+++ mc/missions/models.py
@@ -0,0 +1,15 @@
+import uuid
+from django.db import models
+from django_extensions.db.models import TimeStampedModel
+
+
+class Mission(TimeStampedModel):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                            editable=False)
+    name = models.CharField(null=True, max_length=1024)
+
+class Task(TimeStampedModel):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                            editable=False)
+    name = models.CharField(null=True, max_length=1024)
+    mission = models.ForeignKey('Mission', on_delete=models.CASCADE)
