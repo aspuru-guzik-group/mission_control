f0b07171b052006f4d39ed8943ff01b1ce75e797
adorsk <adorsk@fas.harvard.edu>
2016-11-16 10:12:19 -0500

    start of job model

--- mc/dj/jobs/models.py
+++ mc/dj/jobs/models.py
@@ -1,3 +1,9 @@
+import uuid
 from django.db import models
+from django_extensions.db.models import TimeStampedModel
 
-# Create your models here.
+
+class Job(TimeStampedModel):
+    uuid = models.UUIDField(primary_key=True, default=uuid.uuid4,
+                            editable=False)
+    name = models.CharField(null=True, max_length=1024)
--- mc/dj/jobs/tests.py
+++ mc/dj/jobs/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Job
+
+class JobTestCase(TestCase):
+    def test_has_expected_fields(self):
+        kwargs = {
+            'name': 'test_name',
+        }
+        job = Job.objects.create(**kwargs)
+        self.assertEqual(job.name, kwargs['name'])
+        self.assertTrue(job.uuid is not None)
+        self.assertTrue(job.created is not None)
+        self.assertTrue(job.modified is not None)
