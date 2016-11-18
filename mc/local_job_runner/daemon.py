3bc953a78cd0b303f01124b3464e1035179c1199
adorsk <adorsk@fas.harvard.edu>
2016-11-18 11:40:41 -0500

    rename run => execute

--- mc/local_job_runner/daemon.py
+++ mc/local_job_runner/daemon.py
@@ -5,15 +5,15 @@ import time
 
 class LocalJobRunnerDaemon(object):
     def __init__(self, job_client=None, job_dir_factory=None, interval=120,
-                 max_running_jobs=3, transfer_client=None):
+                 max_executing_jobs=3, transfer_client=None):
         self.job_client = job_client
         self.job_dir_factory = job_dir_factory
         self.interval = interval
-        self.max_running_jobs = max_running_jobs
+        self.max_executing_jobs = max_executing_jobs
         self.transfer_client = transfer_client
 
         self._ticking = False
-        self.running_jobs = {}
+        self.executing_jobs = {}
         self.transferring_jobs = {}
 
     def start(self):
@@ -36,12 +36,12 @@ class LocalJobRunnerDaemon(object):
         time.sleep(self.interval)
 
     def tick(self):
-        num_job_slots = self.max_running_jobs - len(self.running_jobs)
+        num_job_slots = self.max_executing_jobs - len(self.executing_jobs)
         if num_job_slots <= 0: return
         candidate_job_specs = self.fetch_candidate_job_specs()
         for candidate_job_spec in candidate_job_specs[:-num_job_slots]:
             self.process_candidate_job_spec(job_spec=candidate_job_spec)
-        self.process_running_jobs()
+        self.process_executing_jobs()
         self.process_transferring_jobs()
 
     def fetch_candidate_job_specs(self):
@@ -53,9 +53,9 @@ class LocalJobRunnerDaemon(object):
         dir_meta = self.build_job_dir(job_spec=job_spec)
         partial_job = {'key': job_spec['uuid'], 'job_spec': job_spec,
                        'dir': dir_meta}
-        spec_meta = self.start_job_run(job=partial_job)
+        spec_meta = self.start_job_execution(job=partial_job)
         full_job = {**partial_job, 'proc': spec_meta}
-        self.running_jobs[partial_job['key']] = full_job
+        self.executing_jobs[partial_job['key']] = full_job
 
     def claim_job_spec(self, job_spec=None):
         claim_results = self.job_client.claim_jobs(uuids=[job_spec['uuid']])
@@ -65,33 +65,33 @@ class LocalJobRunnerDaemon(object):
         job_dir_meta = self.job_dir_factory.build_job_dir(job_spec=job_spec)
         return job_dir_meta
 
-    def start_job_run(self, job=None):
+    def start_job_execution(self, job=None):
         cmd = 'cd {dir}; {entrypoint}'.format(**job['dir'])
         proc = subprocess.Popen(cmd, shell=True)
         return {'pid': proc.pid}
 
-    def process_running_jobs(self):
-        job_run_states = self.get_job_run_states()
-        completed_jobs = [job for job_key, job in self.running_jobs.items()
-                          if job_run_states[job_key] is None]
-        for completed_job in completed_jobs:
-            self.process_completed_job(job=completed_job)
+    def process_executing_jobs(self):
+        job_execution_states = self.get_job_execution_states()
+        executed_jobs = [job for job_key, job in self.executing_jobs.items()
+                          if job_execution_states[job_key] is None]
+        for executed_job in executed_jobs:
+            self.process_executed_job(job=executed_job)
 
-    def get_job_run_states(self):
-        job_run_states = {}
-        for job_key, job in self.running_jobs.items():
+    def get_job_execution_states(self):
+        job_execution_states = {}
+        for job_key, job in self.executing_jobs.items():
             try:
-                job_run_state = psutil.Process(
+                job_execution_state = psutil.Process(
                     pid=job['proc']['pid']).as_dict()
             except psutil.NoSuchProcess:
-                job_run_state = None
-            job_run_states[job_key] = job_run_state
-        return job_run_states
+                job_execution_state = None
+            job_execution_states[job_key] = job_execution_state
+        return job_execution_states
 
-    def process_completed_job(self, job=None):
+    def process_executed_job(self, job=None):
         transfer_meta = self.start_job_transfer(job=job)
         self.transferring_jobs[job['key']] = {**job, 'transfer': transfer_meta}
-        del self.running_jobs[job['key']]
+        del self.executing_jobs[job['key']]
 
     def start_job_transfer(self, job=None):
         transfer_meta = self.transfer_client.start_transfer(job=job)
@@ -105,7 +105,6 @@ class LocalJobRunnerDaemon(object):
         ]
         for transferred_job in transferred_jobs:
             self.process_transferred_job(job=transferred_job)
-        pass
 
     def get_job_transfer_states(self):
         job_transfer_states = {}
--- mc/local_job_runner/tests/test_daemon.py
+++ mc/local_job_runner/tests/test_daemon.py
@@ -31,7 +31,7 @@ class TickTestCase(DaemonBaseTestCase):
         super().setUp()
         self.patcher = patch.multiple(
             self.daemon, fetch_candidate_job_specs=DEFAULT,
-            process_candidate_job_spec=DEFAULT, process_running_jobs=DEFAULT,
+            process_candidate_job_spec=DEFAULT, process_executing_jobs=DEFAULT,
             process_transferring_jobs=DEFAULT)
         self.mocks = self.patcher.start()
 
@@ -43,10 +43,12 @@ class TickTestCase(DaemonBaseTestCase):
         self.assertTrue(self.mocks['fetch_candidate_job_specs'].called)
 
     def test_processes_candidate_job_specs(self):
-        self.daemon.max_running_jobs = 3
+        self.daemon.max_executing_jobs = 3
         extra_jobs = 2
-        job_specs = [{'uuid': i}
-                     for i in range(self.daemon.max_running_jobs + extra_jobs)]
+        job_specs = [
+            {'uuid': i}
+            for i in range(self.daemon.max_executing_jobs + extra_jobs)
+        ]
         self.mocks['fetch_candidate_job_specs'].return_value = job_specs
         self.daemon.tick()
         expected_call_args = [call(job_spec=job_spec)
@@ -54,9 +56,9 @@ class TickTestCase(DaemonBaseTestCase):
         self.assertTrue(self.mocks['process_candidate_job_spec'].call_args_list,
                         expected_call_args)
 
-    def test_processes_running_jobs(self):
+    def test_processes_executing_jobs(self):
         self.daemon.tick()
-        self.assertEqual(self.mocks['process_running_jobs'].call_count, 1)
+        self.assertEqual(self.mocks['process_executing_jobs'].call_count, 1)
 
     def test_processes_transferring_jobs(self):
         self.daemon.tick()
@@ -71,7 +73,7 @@ class ProcessCandidateJobSpecTestCase(DaemonBaseTestCase):
     def setUp(self):
         super().setUp()
         self.patcher = patch.multiple(self.daemon, build_job_dir=DEFAULT,
-                                      start_job_run=DEFAULT)
+                                      start_job_execution=DEFAULT)
         self.mocks = self.patcher.start()
 
     def tearDown(self):
@@ -82,25 +84,26 @@ class ProcessCandidateJobSpecTestCase(DaemonBaseTestCase):
         self.job_client.claim_jobs.return_value = {job_spec['uuid']: True}
         self.daemon.process_candidate_job_spec(job_spec=job_spec)
         expected_job_dir_meta = self.mocks['build_job_dir'].return_value
-        expected_job_proc_meta = self.mocks['start_job_run'].return_value
+        expected_job_proc_meta = self.mocks['start_job_execution'].return_value
         self.assertEqual(self.mocks['build_job_dir'].call_count, 1)
         expected_partial_job = {
             'key': job_spec['uuid'],
             'job_spec': job_spec,
             'dir': expected_job_dir_meta,
         }
-        self.assertEqual(self.mocks['start_job_run'].call_args,
+        self.assertEqual(self.mocks['start_job_execution'].call_args,
                          call(job=expected_partial_job))
         expected_full_job = {**expected_partial_job,
                              'proc': expected_job_proc_meta}
-        self.assertEqual(self.daemon.running_jobs[expected_partial_job['key']],
-                         expected_full_job)
+        self.assertEqual(
+            self.daemon.executing_jobs[expected_partial_job['key']],
+            expected_full_job)
 
     def test_unclaimable_job_spec(self):
         job_spec = {'uuid': 'abcd'}
         self.job_client.claim_jobs.return_value = {job_spec['uuid']: False}
         self.daemon.process_candidate_job_spec(job_spec)
-        self.assertEqual(self.mocks['start_job_run'].call_count, 0)
+        self.assertEqual(self.mocks['start_job_execution'].call_count, 0)
 
 class BuildJobDirTestCase(DaemonBaseTestCase):
     def test_build_job_dir(self):
@@ -109,7 +112,7 @@ class BuildJobDirTestCase(DaemonBaseTestCase):
         self.assertTrue(self.daemon.job_dir_factory.build_job_dir.call_args,
                         call(job_spec=job_spec))
 
-class RunJobTestCase(DaemonBaseTestCase):
+class StartJobExecutionTestCase(DaemonBaseTestCase):
     def setUp(self):
         super().setUp()
         self.patcher = patch('subprocess.Popen')
@@ -118,13 +121,13 @@ class RunJobTestCase(DaemonBaseTestCase):
     def tearDown(self):
         self.patcher.stop()
 
-    def test_start_job_run(self):
+    def test_start_job_execution(self):
         # calls Popen w/ cd job dir, command.
         job = {
             'key': 'abcd',
             'dir': {'dir': 'mock_dir', 'entrypoint': 'job.sh'},
         }
-        proc_meta = self.daemon.start_job_run(job=job)
+        proc_meta = self.daemon.start_job_execution(job=job)
         expected_cmd = 'cd {dir}; {entrypoint}'.format(**job['dir'])
         expected_popen_call = call(expected_cmd, shell=True)
         self.assertEqual(self.mocks['subprocess.Popen'].call_args,
@@ -133,32 +136,32 @@ class RunJobTestCase(DaemonBaseTestCase):
             'pid': self.mocks['subprocess.Popen'].return_value.pid}
         self.assertEqual(proc_meta, expected_proc_meta)
 
-class ProcessRunningJobsTestCase(DaemonBaseTestCase):
+class ProcessExecutingJobsTestCase(DaemonBaseTestCase):
     def setUp(self):
         super().setUp()
-        self.completed_jobs = [{'key': 'completed_%s' % i} for i in range(3)]
-        self.running_jobs = [{'key': 'running_%s' % i} for i in range(3)]
-        self.daemon.running_jobs = {
-            j['key']: j for j in self.completed_jobs + self.running_jobs
+        self.executed_jobs = [{'key': 'executed_%s' % i} for i in range(3)]
+        self.executing_jobs = [{'key': 'executing_%s' % i} for i in range(3)]
+        self.daemon.executing_jobs = {
+            j['key']: j for j in self.executed_jobs + self.executing_jobs
         }
-        self.mock_job_run_states = {
-            **{j['key']: None for j in self.completed_jobs},
-            **{j['key']: {'mock': 'state'} for j in self.running_jobs}
+        self.mock_job_execution_states = {
+            **{j['key']: None for j in self.executed_jobs},
+            **{j['key']: {'mock': 'state'} for j in self.executing_jobs}
         }
         self.patcher = patch.multiple(self.daemon,
-                                      get_job_run_states=DEFAULT,
-                                      process_completed_job=DEFAULT)
+                                      get_job_execution_states=DEFAULT,
+                                      process_executed_job=DEFAULT)
         self.mocks = self.patcher.start()
-        self.mocks['get_job_run_states'].return_value = \
-                self.mock_job_run_states
+        self.mocks['get_job_execution_states'].return_value = \
+                self.mock_job_execution_states
 
     def tearDown(self):
         self.patcher.stop()
 
-    def test_process_running_jobs(self):
-        self.daemon.process_running_jobs()
-        expected_calls = [call(job=job) for job in self.completed_jobs]
-        calls = self.mocks['process_completed_job'].mock_calls
+    def test_process_executing_jobs(self):
+        self.daemon.process_executing_jobs()
+        expected_calls = [call(job=job) for job in self.executed_jobs]
+        calls = self.mocks['process_executed_job'].mock_calls
         calls_sort_key_fn = lambda c: c[2]['job']['key']
         sorted_calls = sorted(calls, key=calls_sort_key_fn)
         sorted_expected_calls = sorted(expected_calls, key=calls_sort_key_fn)
@@ -170,20 +173,20 @@ class ProcessCompletedJobTestCase(DaemonBaseTestCase):
         self.patcher = patch.multiple(self.daemon, start_job_transfer=DEFAULT)
         self.mocks = self.patcher.start()
         self.job_key = 'abcd'
-        self.completed_job = {'key': self.job_key}
-        self.daemon.running_jobs[self.job_key] = self.completed_job
+        self.executed_job = {'key': self.job_key}
+        self.daemon.executing_jobs[self.job_key] = self.executed_job
 
     def tearDown(self):
         self.patcher.stop()
 
-    def test_transfers_completed_jobs(self):
-        self.daemon.process_completed_job(job=self.completed_job)
+    def test_transfers_executed_jobs(self):
+        self.daemon.process_executed_job(job=self.executed_job)
         self.assertEqual(self.mocks['start_job_transfer'].call_args_list,
-                         [call(job=self.completed_job)])
-        self.assertTrue(self.job_key not in self.daemon.running_jobs)
+                         [call(job=self.executed_job)])
+        self.assertTrue(self.job_key not in self.daemon.executing_jobs)
         self.assertEqual(
             self.daemon.transferring_jobs[self.job_key],
-            {**self.completed_job,
+            {**self.executed_job,
              'transfer': self.mocks['start_job_transfer'].return_value})
 
 class GetJobRunStatesTestCase(DaemonBaseTestCase):
@@ -192,26 +195,26 @@ class GetJobRunStatesTestCase(DaemonBaseTestCase):
         self.patcher = patch('psutil.Process')
         self.mocks = {'psutil.Process': self.patcher.start()}
 
-        self.daemon.running_jobs = {}
+        self.daemon.executing_jobs = {}
         for i in range(3):
             job_key = 'job_key_%s' % i
-            self.daemon.running_jobs[job_key] = {'key': job_key,
+            self.daemon.executing_jobs[job_key] = {'key': job_key,
                                                  'proc': {'pid': i}}
 
     def tearDown(self):
         self.patcher.stop()
 
-    def test_get_job_run_states(self):
-        job_run_states = self.daemon.get_job_run_states()
+    def test_get_job_execution_states(self):
+        job_execution_states = self.daemon.get_job_execution_states()
         expected_psutil_calls = [call(pid=j['proc']['pid'])
-                                 for j in self.daemon.running_jobs.values()]
+                                 for j in self.daemon.executing_jobs.values()]
         self.assertEqual(self.mocks['psutil.Process'].call_args_list,
                          expected_psutil_calls)
-        mock_job_run_state = operator.attrgetter(
+        mock_job_execution_state = operator.attrgetter(
             'return_value.as_dict.return_value')(self.mocks['psutil.Process'])
-        expected_job_states = {job_key: mock_job_run_state
-                               for job_key in self.daemon.running_jobs.keys()}
-        self.assertEqual(job_run_states, expected_job_states)
+        expected_job_states = {job_key: mock_job_execution_state
+                               for job_key in self.daemon.executing_jobs.keys()}
+        self.assertEqual(job_execution_states, expected_job_states)
 
 class TransferJobTestCase(DaemonBaseTestCase):
     def setUp(self):
