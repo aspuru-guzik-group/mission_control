c890f14015c0b45e8b3fc95d475a677b14c5d221
adorsk <adorsk@fas.harvard.edu>
2016-12-19 11:07:48 -0500

    adding child node methods, get_nearest_incomplete to workflow

--- mc/dj/workflow_runners/tests/test_workflow.py
+++ mc/dj/workflow_runners/tests/test_workflow.py
@@ -1,11 +1,7 @@
 import unittest
-from unittest.mock import call, DEFAULT, patch
+from unittest.mock import call, DEFAULT, patch, MagicMock
 
 from ..workflow import Workflow
-
-class StubNode(object):
-    def __init__(self, id=None):
-        self.id = id
 
 
 class BaseTestCase(unittest.TestCase):
@@ -25,8 +21,13 @@ class BaseTestCase(unittest.TestCase):
     def generate_nodes(self, n=3):
         nodes = []
         for i in range(n):
-            nodes.append(StubNode(id=i))
+            nodes.append(self.generate_node(id=i))
         return nodes
+
+    def generate_node(self, **kwargs):
+        node = MagicMock()
+        node.configure_mock(**kwargs)
+        return node
 
 class AddNodesTestCase(BaseTestCase):
     def setUp(self):
@@ -44,7 +45,7 @@ class AddNodesTestCase(BaseTestCase):
 
 class AddNodeTestCase(BaseTestCase):
     def test_adds_node(self):
-        node = StubNode(id='id')
+        node = self.generate_node()
         self.assertFalse(node.id in self.workflow.nodes)
         self.workflow.add_node(node=node)
         self.assertEqual(self.workflow.nodes[node.id], node)
@@ -64,13 +65,102 @@ class AddEdgesTestCase(BaseTestCase):
                          [call(edge=edge) for edge in edges])
 
 class AddEdgeTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.nodes = self.generate_nodes(n=2)
+        self.edge = {'src': self.nodes[0], 'dest': self.nodes[1]}
+        self.expected_edge_key = (self.edge['src'].id, self.edge['dest'].id)
+
     def test_add_edge(self):
+        self.assertFalse(self.expected_edge_key in self.workflow.edges)
+        self.workflow.add_edge(edge=self.edge)
+        self.assertEqual(self.workflow.edges[self.expected_edge_key], self.edge)
+
+    def test_updates_edges_by_node_id(self):
+        self.assertTrue(self.nodes[0].id not in self.workflow.edges_by_node_id)
+        self.assertTrue(self.nodes[1].id not in self.workflow.edges_by_node_id)
+        self.workflow.add_edge(edge=self.edge)
+        for node in self.nodes:
+            self.assertEqual(
+                self.workflow.edges_by_node_id[node.id][self.expected_edge_key],
+                self.edge)
+
+class ConnectNodesTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.patchers = {
+            'workflow': patch.multiple(self.workflow, add_edge=DEFAULT)
+        }
+        self.mocks = self.start_patchers(self.patchers)
+
+    def test_connect_nodes(self):
         nodes = self.generate_nodes(n=2)
-        edge = {'src': nodes[0], 'dest': nodes[1]}
-        expected_edge_key = (edge['src'].id, edge['dest'].id)
-        self.assertFalse(expected_edge_key in self.workflow.edges)
-        self.workflow.add_edge(edge=edge)
-        self.assertEqual(self.workflow.edges[expected_edge_key], edge)
+        self.workflow.add_nodes(nodes=nodes)
+        self.workflow.connect_nodes(src=nodes[0], dest=nodes[1])
+        self.assertEqual(self.mocks['workflow']['add_edge'].call_args,
+                         call(edge={'src': nodes[0], 'dest': nodes[1]}))
+
+class GetNearestIncompleteNodesTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.setup_workflow()
+
+    def setup_workflow(self):
+        self.workflow.root_node = self.generate_node(id='ROOT',
+                                                     status='COMPLETED')
+        self.linear_branches = {
+            '1': [
+                self.generate_node(id='1.1', status='COMPLETED'),
+                self.generate_node(id='1.2', status='PENDING'),
+                self.generate_node(id='1.3', status='PENDING')
+            ],
+            '2': [
+                self.generate_node(id='2.1', status='COMPLETED'),
+                self.generate_node(id='2.2', status='COMPLETED'),
+                self.generate_node(id='2.3', status='PENDING'),
+                self.generate_node(id='2.4', status='PENDING'),
+            ],
+        }
+        for branch_nodes in self.linear_branches.values():
+            self.add_linear_branch_to_workflow(workflow=self.workflow,
+                                               branch_nodes=branch_nodes)
+
+    def add_linear_branch_to_workflow(self, workflow=None, branch_nodes=None):
+        tail = workflow.root_node
+        for node in branch_nodes:
+            workflow.add_nodes(nodes=[node])
+            workflow.connect_nodes(src=tail, dest=node)
+            tail = node
+
+    def test_get_nearest_incomplete_nodes(self):
+        expected_nearest_incomplete_nodes = [self.linear_branches['1'][1],
+                                             self.linear_branches['2'][2]]
+        self.assertEqual(set(self.workflow.get_nearest_incomplete_nodes()),
+                         set(expected_nearest_incomplete_nodes))
+
+class GetChildNodes(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.setup_workflow(workflow=self.workflow)
+
+    def setup_workflow(self, workflow=None):
+        workflow.root_node = self.generate_node(id='ROOT')
+        node_1 = self.generate_node(id='1')
+        self.workflow.add_node(node_1)
+        workflow.connect_nodes(src=workflow.root_node, dest=node_1)
+        node_1_1 = self.generate_node(id='1_1')
+        self.workflow.add_node(node_1_1)
+        workflow.connect_nodes(src=node_1, dest=node_1_1)
+        node_1_2 = self.generate_node(id='1_2')
+        self.workflow.add_node(node_1_2)
+        workflow.connect_nodes(src=node_1, dest=node_1_2)
+
+    def test_gets_child_nodes(self):
+        expected_child_nodes = [self.workflow.nodes['1_1'],
+                                self.workflow.nodes['1_2']]
+        child_nodes = self.workflow.get_child_nodes(
+            parent_node=self.workflow.nodes['1'])
+        self.assertEqual(set(child_nodes), set(expected_child_nodes))
 
 if __name__ == '__main__':
     unittest.main()
--- mc/dj/workflow_runners/workflow.py
+++ mc/dj/workflow_runners/workflow.py
@@ -1,11 +1,21 @@
+from collections import defaultdict
+from uuid import uuid4
 
+
+class StaticNode(object):
+    def __init__(self, id=None, status=None, state=None, **kwargs):
+        self.id = id or str(uuid4())
+        self.status = status
+        self.state = state
 
 class Workflow(object):
-    def __init__(self):
+    def __init__(self, root_node=None):
         self.nodes = {}
         self.edges = {}
+        self.edges_by_node_id = defaultdict(dict)
         self.state = None
         self.status = None
+        self.root_node = root_node
 
     def add_nodes(self, nodes=None):
         for node in nodes: self.add_node(node=node)
@@ -18,5 +28,37 @@ class Workflow(object):
         for edge in edges: self.add_edge(edge=edge)
 
     def add_edge(self, edge=None):
-        edge_key = (edge['src'].id, edge['dest'].id)
+        src, dest = [edge[k] for k in ('src','dest')]
+        edge_key = (src.id, dest.id)
         self.edges[edge_key] = edge
+        self.edges_by_node_id[src.id][edge_key] = edge
+        self.edges_by_node_id[dest.id][edge_key] = edge
+
+    def connect_nodes(self, src=None, dest=None):
+        edge = {'src': src, 'dest': dest}
+        self.add_edge(edge=edge)
+
+    def add_child_nodes(self, parent_node=None, child_nodes=None):
+        self.add_nodes(nodes=child_nodes)
+        for child_node in child_nodes:
+            self.connect_nodes(src=parent_node, dest=child_node)
+
+    def get_child_nodes(self, parent_node=None):
+        parent_node_edges = self.edges_by_node_id[parent_node.id].values()
+        child_nodes = [edge['dest'] for edge in parent_node_edges
+                       if edge['src'] is parent_node]
+        return child_nodes
+
+    def get_nearest_incomplete_nodes(self):
+        nearest_incomplete_nodes = []
+        cursors = [self.root_node]
+        while len(cursors) > 0:
+            next_cursors = []
+            for cursor in cursors:
+                if cursor.status == 'COMPLETED':
+                    child_nodes = self.get_child_nodes(parent_node=cursor)
+                    next_cursors.extend(child_nodes)
+                else:
+                    nearest_incomplete_nodes.append(cursor)
+            cursors = next_cursors
+        return nearest_incomplete_nodes
