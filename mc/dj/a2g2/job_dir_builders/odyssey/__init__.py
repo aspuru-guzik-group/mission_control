6ae42d0e1f452e2d13ed19b6f9dd58b935c67b6d
adorsk <adorsk@fas.harvard.edu>
2017-01-10 15:53:34 -0500

    add template logic to ody builder

--- mc/dj/a2g2/job_dir_builders/odyssey/__init__.py
+++ mc/dj/a2g2/job_dir_builders/odyssey/__init__.py
@@ -3,16 +3,19 @@ import os
 import tempfile
 import textwrap
 
+from jinja2 import Template
+
 
 class OdysseyJobDirBuilder(object):
     @classmethod
     def build_dir(cls, dir_spec=None, output_dir=None):
         if not output_dir: output_dir = tempfile.mkdtemp(prefix='odyssey.')
-        cls.write_job_script(dir_spec=dir_spec,
-                             script_path=os.path.join(output_dir, 'job.sh'))
+        cls.write_job_script(dir_spec=dir_spec, output_dir=output_dir)
+        cls.write_templates(dir_spec=dir_spec, output_dir=output_dir)
 
     @classmethod
-    def write_job_script(cls, dir_spec=None, script_path=None):
+    def write_job_script(cls, dir_spec=None, output_dir=None):
+        script_path = os.path.join(output_dir, 'job.sh')
         script_content = cls.generate_job_script_content(dir_spec=dir_spec)
         with open(script_path, 'w') as f: f.write(script_content)
 
@@ -59,11 +62,51 @@ class OdysseyJobDirBuilder(object):
 
     @classmethod
     def generate_module_section(cls, dir_spec=None):
-        modules = cls.flatten_kvp_list(dir_spec.get('modules', []))
+        modules = cls.prepare_modules(modules=dir_spec.get('modules'))
         module_lines = ["module load {module}".format(module=module)
                         for module, include in modules if include]
         return "\n".join(module_lines)
 
     @classmethod
+    def prepare_modules(cls, modules=None):
+        prepared_modules = []
+        for module in (modules or []):
+            if isinstance(module, str): module = [module, True]
+            prepared_modules.append(module)
+        prepared_modules = cls.flatten_kvp_list(prepared_modules)
+        return prepared_modules
+
+    @classmethod
     def generate_body_section(cls, dir_spec=None):
         return dir_spec.get('job_script_body', '')
+
+    @classmethod
+    def write_templates(cls, dir_spec=None, output_dir=None):
+        templates = dir_spec.get('templates', {})
+        if not templates.get('specs', None): return
+        ctx = templates.get('ctx', {})
+        for template_spec in templates.get('specs', []):
+            cls.write_template_spec(template_spec=template_spec,
+                                    output_dir=output_dir,
+                                    ctx=ctx)
+
+    @classmethod
+    def write_template_spec(cls, template_spec=None, output_dir=None, ctx=None):
+        target_path = os.path.join(output_dir, template_spec['target'])
+        cls.ensure_dir(dir_path=os.path.dirname(target_path))
+        rendered = cls.render_template_spec(template_spec=template_spec,
+                                            ctx=ctx)
+        with open(target_path, 'w') as f: f.write(rendered + "\n")
+
+    @classmethod
+    def render_template_spec(cls, template_spec=None, ctx=None):
+        template = Template(template_spec['content'])
+        template.environment.keep_trailing_newline = True
+        merged_ctx = {**(ctx or {}), **template_spec.get('ctx', {})}
+        rendered = template.render(merged_ctx)
+        return rendered
+
+    @classmethod
+    def ensure_dir(cls, dir_path=None):
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
