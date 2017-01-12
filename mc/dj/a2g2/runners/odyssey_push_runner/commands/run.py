942f34e20421c01c6c5301f1d565460e03971a33
adorsk <adorsk@fas.harvard.edu>
2017-01-12 14:42:05 -0500

    pass down login params via command

--- mc/dj/a2g2/runners/odyssey_push_runner/commands/run.py
+++ mc/dj/a2g2/runners/odyssey_push_runner/commands/run.py
@@ -2,6 +2,10 @@ from .base import BaseCommand
 from .. import odyssey_push_runner
 
 class Command(BaseCommand):
+    def add_arguments(self, parser):
+        parser.add_argument('--odyssey_user', help="Odyssey user")
+        parser.add_argument('--odyssey_host', help="Odyssey host")
+
     def handle(self, *args, **options):
-        self.runner = odyssey_push_runner.OdysseyPushRunner()
+        self.runner = odyssey_push_runner.OdysseyPushRunner(**options)
         self.runner.run()
