134560b9f8785c155cb53e3544c9d6c8b29ccd8c
adorsk <adorsk@fas.harvard.edu>
2017-01-17 15:51:09 -0500

    factoring out common e2e stuff, starting runner e2e test

--- mc/dj/a2g2/runners/odyssey_push_runner/tests/test_create_flow_record_e2e.py
+++ mc/dj/a2g2/runners/odyssey_push_runner/tests/test_create_flow_record_e2e.py
@@ -1,46 +1,21 @@
 import io
 import json
 import shlex
-import unittest
-from unittest.mock import patch
 
-from django.conf.urls import url, include
-from django.test import TestCase, override_settings
+from django.test import override_settings
 
 from missions.models import Flow
 
 from ..commands.create_flow_record import Command as CreateFlowRecordCommand
-from ..commands import base as base_command
 
+from . import e2e_utils
 
-BASE_PATH = 'test_api'
-urlpatterns = [
-    url(r'^%s/' % BASE_PATH, include('missions.urls')),
-]
+urlpatterns = e2e_utils.urlpatterns
+assert urlpatterns
 
-class BaseTestCase(TestCase):
-    def setUp(self):
-        self.configure_request_client()
-        self.request_client_patcher = patch.object(
-            base_command, 'request_client', new=self.client)
-        self.request_client_patcher.start()
-
-    def configure_request_client(self):
-        for method_name in ['patch', 'post']:
-            self.patch_client_method_to_use_json(method_name=method_name)
-
-    def patch_client_method_to_use_json(self, method_name=None):
-        orig_method = getattr(self.client, method_name)
-        def patched_method(path, data=None, **kwargs):
-            return orig_method(path, json.dumps(data), 
-                               content_type='application/json', **kwargs)
-        setattr(self.client, method_name, patched_method)
-
-    def tearDown(self):
-        self.request_client_patcher.stop()
 
 @override_settings(ROOT_URLCONF=__name__)
-class CreateFlowRecordE2ETestCase(BaseTestCase):
+class CreateFlowRecordE2ETestCase(e2e_utils.BaseTestCase):
     def setUp(self):
         super().setUp()
 
@@ -53,7 +28,7 @@ class CreateFlowRecordE2ETestCase(BaseTestCase):
     def run_create_flow_record_command(self, flow_spec=None):
         arg_strs = [
             "--flow_spec_json='%s'" % json.dumps(flow_spec),
-            "--flow_server_url='/%s/'" % BASE_PATH,
+            "--flow_server_url='/%s/'" % e2e_utils.BASE_PATH,
             "--job_server_url=''",
         ]
         args = shlex.split(' '.join(arg_strs))
@@ -66,8 +41,3 @@ class CreateFlowRecordE2ETestCase(BaseTestCase):
     def assert_flow_created(self, flow=None, flow_spec=None):
         flow_record = Flow.objects.get(uuid=flow['uuid'])
         self.assertEqual(flow_record.spec, json.dumps(flow_spec))
-
-@unittest.skip
-class RunFlowTestCase(TestCase):
-    def test_runs_flow(self):
-        self.fail()
---
+++ mc/dj/a2g2/runners/odyssey_push_runner/tests/test_odyssey_push_runner_e2e.py
@@ -0,0 +1,163 @@
+import json
+import unittest
+from unittest.mock import Mock
+
+from django.test import override_settings
+
+from missions.models import Flow as FlowModel
+from ..odyssey_push_runner import OdysseyPushRunner
+from . import e2e_utils
+from flow_engines.flow import Flow
+from a2g2.tasks.job import JobTask
+from a2g2.tasks.flow import FlowTask
+from jobs.models import Job
+
+
+urlpatterns = e2e_utils.urlpatterns
+assert urlpatterns
+
+
+@override_settings(ROOT_URLCONF=__name__)
+class OdysseyPushRunnerE2ETestCase(e2e_utils.BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.execution_client = self.generate_mock_execution_client()
+        self.transfer_client = Mock()
+        self.flow_generator_classes = self.generate_flow_generator_classes()
+        self.runner = self.generate_runner()
+        self.flow_spec = self.generate_flow_spec()
+        self.parent_flow_model = self.generate_flow_model()
+
+    def generate_mock_execution_client(self):
+        mock_execution_client = Mock()
+        return mock_execution_client
+
+    def generate_mock_transfer_client(self):
+        mock_transfer_client = Mock()
+        return mock_transfer_client
+
+    def generate_flow_generator_classes(self):
+        flow_generator_classes = {}
+        class FlowGeneratorA:
+            flow_type = 'FlowA'
+
+            @classmethod
+            def generate_flow(cls, *args, flow_spec=None, **kwargs):
+                flow = Flow()
+                flow.add_task(
+                    key='a.1',
+                    as_root=True,
+                    task=JobTask(),
+                    input={'job_spec': {'type': 'Job_A'}}
+                )
+                return flow
+
+            @classmethod
+            def get_dependencies(cls):
+                return {
+                    'task_classes': set([JobTask])
+                }
+        flow_generator_classes['A'] = FlowGeneratorA
+
+        class FlowGeneratorB:
+            flow_type = 'FlowB'
+
+            @classmethod
+            def generate_flow(cls, *args, flow_spec=None, **kwargs):
+                flow = Flow()
+                flow.add_task(
+                    key='b.1',
+                    as_root=True,
+                    task=FlowTask(),
+                    input={
+                        'flow_spec': {'flow_type': FlowGeneratorA.flow_type}
+                    }
+                )
+                return flow
+
+            @classmethod
+            def get_dependencies(cls):
+                return {
+                    'task_classes': set([FlowTask]),
+                    'flow_generator_classes': set([FlowGeneratorA])
+                }
+        flow_generator_classes['B'] = FlowGeneratorB
+        return flow_generator_classes
+
+    def generate_runner(self):
+        mc_server_url = '/%s/' % e2e_utils.BASE_PATH
+        runner = OdysseyPushRunner(
+            request_client=self.client,
+            run_setup=True,
+            job_server_url=mc_server_url,
+            flow_server_url=mc_server_url,
+            flow_generator_classes=self.flow_generator_classes.values(),
+        )
+        runner.job_runner.execution_client = self.execution_client
+        runner.job_runner.transfer_client = self.execution_client
+        return runner
+
+    def generate_flow_spec(self):
+        flow_spec = {'flow_type': self.flow_generator_classes['B'].flow_type}
+        return flow_spec
+
+    def generate_flow_model(self):
+        flow_model = FlowModel.objects.create(
+            spec=json.dumps(self.flow_spec)
+        )
+        return flow_model
+
+    def test_runs_flows(self):
+        self.runner.tick()
+        self.assert_parent_flow_model_attr('status', 'RUNNING')
+        self.child_flow_model = self.get_child_flow_model()
+        self.assertTrue(self.child_flow_model is not None)
+
+        self.runner.tick()
+        self.assert_parent_flow_model_attr('status', 'RUNNING')
+        self.job_model = self.get_job_model()
+        self.assertTrue(self.job_model is not None)
+
+        self.runner.tick()
+        self.assert_job_model_running()
+        self.assert_child_flow_model_running()
+        self.assert_parent_flow_model_running()
+
+        self.mock_job_execution()
+
+        self.runner.tick()
+        self.assert_job_model_completed()
+        self.assert_child_flow_model_completed()
+        self.assert_parent_flow_model_running()
+
+        self.runner.tick()
+        self.assert_parent_flow_model_completed()
+
+    def get_parent_flow_model(self):
+        return FlowModel.objects.get(uuid=self.parent_flow_model.uuid)
+
+    def assert_parent_flow_model_attr(self, attr, expected):
+        parent_flow_model = self.get_parent_flow_model()
+        self.assertEqual(getattr(parent_flow_model, attr), expected)
+
+    def get_child_flow_model(self):
+        return FlowModel.objects.exclude(uuid=self.parent_flow_model.uuid)[0]
+
+    def assert_child_flow_model_attr(self, attr, expected):
+        child_flow_model = self.get_child_flow_model()
+        self.assertEqual(getattr(child_flow_model, attr), expected)
+
+    def get_job_model(self):
+        job_models = Job.objects.all()
+        job_model = filter(job_models, lambda j: j['spec']['type'] == 'A')[0]
+        return job_model
+
+    def assert_job_model_attr(self, attr, expected):
+        job_model = self.get_job_model()
+        self.assertEqual(getattr(job_model, attr), expected)
+
+    def mock_job_execution(self):
+        raise NotImplementedError
+
+if __name__ == '__main__':
+    unittest.main()
