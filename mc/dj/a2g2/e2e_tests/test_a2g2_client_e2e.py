848133097d7852955960fe5dcdfba54ee88c9d87
adorsk <adorsk@fas.harvard.edu>
2017-01-18 15:55:20 -0500

    add a2g2_client e2e

--- mc/dj/a2g2/e2e_tests/test_a2g2_client_e2e.py
+++ mc/dj/a2g2/e2e_tests/test_a2g2_client_e2e.py
@@ -1,18 +1,39 @@
-from django.test import TestCase
+from django.conf.urls import url, include
+from django.test import TestCase, override_settings
 
 from mc_utils import test_utils
 
-from .a2g2_client import A2G2_Client
+from ..a2g2_dj.models import Mol
+from ..a2g2_dj import urls as _a2g2_dj_urls
+from ..a2g2_client.a2g2_client import A2G2_Client
+
+BASE_PATH = 'test_api'
+urlpatterns = [
+    url(r'^%s' % BASE_PATH, include(_a2g2_dj_urls.__name__)),
+]
 
 class BaseTestCase(TestCase):
     def setUp(self):
-        test_utils.patch_request_client(request_client=self.client)
-        self.a2g2_client = A2G2_Client(request_client=self.client)
+        test_utils.patch_request_client(request_client=self.client,
+                                        json_methods=['get', 'post', 'patch'])
 
+        self.a2g2_client = A2G2_Client(base_url='/' + BASE_PATH,
+                                       request_client=self.client)
+
+@override_settings(ROOT_URLCONF=__name__)
 class CreateMolsTestCase(BaseTestCase):
-    def test_creates_mols(self): pass
+    def test_creates_mols(self):
+        self.assertEqual(Mol.objects.count(), 0)
+        self.a2g2_client.create_mol(mol={})
+        self.assertEqual(Mol.objects.count(), 1)
 
+    def test_response_has_uuid(self):
+        created_mol = self.a2g2_client.create_mol(mol={})
+        self.assertTrue(created_mol['uuid'] is not None)
+
+@override_settings(ROOT_URLCONF=__name__)
 class GetCountsTestCase(BaseTestCase):
     def test_gets_mol_counts(self):
-        mols = self.a2g2_client.create_mols(mols=[{} for i in range(3)])
-        self.assertEqual(self.a2g2_client.get_counts().get('Mol'), len(mols))
+        for i in range(3): Mol.objects.create()
+        self.assertEqual(self.a2g2_client.get_counts().get('Mol'),
+                         Mol.objects.count())
--- mc/dj/mc_utils/test_utils.py
+++ mc/dj/mc_utils/test_utils.py
@@ -2,14 +2,25 @@ import json
 from unittest.mock import Mock
 
 
-def patch_request_client(client=None, json_methods=['patch', 'post']):
+def patch_request_client(request_client=None, json_methods=['patch', 'post']):
     for method_name in json_methods:
-        patch_client_method_to_use_json(client=client, method_name=method_name)
-    client.raise_for_status = Mock()
+        patch_client_method_to_use_json(client=request_client,
+                                        method_name=method_name)
+    request_client.raise_for_status = Mock()
 
 def patch_client_method_to_use_json(client=None, method_name=None):
     orig_method = getattr(client, method_name)
     def patched_method(path, data=None, **kwargs):
-        return orig_method(path, json.dumps(data), 
-                           content_type='application/json', **kwargs)
+        if method_name == 'get':
+            response = orig_method(path, content_type='application/json',
+                                   **kwargs)
+        else:
+            response =  orig_method(path, json.dumps(data), 
+                                    content_type='application/json', **kwargs)
+        def raise_for_status():
+            if not str(response.status_code).startswith('2'):
+                raise Exception("Bad response: ", response)
+        response.raise_for_status = raise_for_status
+        return response
     setattr(client, method_name, patched_method)
+
