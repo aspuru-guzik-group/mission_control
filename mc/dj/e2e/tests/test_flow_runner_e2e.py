ec22286d8d8c99713e9ff43b23aa497b6c4820b1
adorsk <adorsk@fas.harvard.edu>
2017-02-01 14:05:04 -0500

    update flow_runner test for new flow_engine api changes

--- mc/dj/e2e/tests/test_flow_runner_e2e.py
+++ mc/dj/e2e/tests/test_flow_runner_e2e.py
@@ -19,9 +19,9 @@ urlpatterns = [
 class FlowRunnerE2ETestCase(TestCase):
     def setUp(self):
         test_utils.patch_request_client(request_client=self.client)
-        self.keyed_task_classes = self.generate_keyed_task_classes()
+        self.keyed_task_engines = self.generate_keyed_task_engines()
         self.flow_engine = self.generate_flow_engine(
-            keyed_task_classes=self.keyed_task_classes)
+            keyed_task_engines=self.keyed_task_engines)
         self.flow_client = MissionControlFlowClient(
             base_url='/%s' % BASE_PATH,
             request_client=self.client
@@ -31,65 +31,69 @@ class FlowRunnerE2ETestCase(TestCase):
             flow_engine=self.flow_engine)
         self.populate_flows()
 
-    def generate_keyed_task_classes(self):
-        class BaseTestTask():
+    def generate_keyed_task_engines(self):
+        class BaseTaskEngine():
             def __init__(self, *args, **kwargs):
                 self.__dict__.update(kwargs)
 
-            def tick(self, *args, **kwargs):
-                if not hasattr(self.__class__, 'ticks'):
-                    self.__class__.ticks = 0
-                self.__class__.ticks += 1
+            @classmethod
+            def tick_task(cls, *args, **kwargs):
+                if not hasattr(cls, 'ticks'):
+                    cls.ticks = 0
+                cls.ticks += 1
 
-        task_classes = []
-        class Task_0(BaseTestTask):
-            def tick(self, *args, **kwargs):
-                super().tick()
-                next_task = Task_1(status='PENDING')
-                self.flow.add_task(next_task, precursor=self)
-                self.status = 'COMPLETED'
-        task_classes.append(Task_0)
+        task_engines = []
+        class TaskEngine_0(BaseTaskEngine):
+            @classmethod
+            def tick_task(cls, *args, task=None, ctx=None, **kwargs):
+                super().tick_task(*args, task=task, **kwargs)
+                next_task = {'task_engine': TaskEngine_1.__name__,
+                             'status': 'PENDING'}
+                ctx['flow'].add_task(next_task, precursor_keys=[task['key']])
+                task['status'] = 'COMPLETED'
+        task_engines.append(TaskEngine_0)
 
-        class Task_1(BaseTestTask):
+        class TaskEngine_1(BaseTaskEngine):
             allow_completion = False
 
-            def tick(self, *args, **kwargs):
-                super().tick()
-                if self.__class__.allow_completion:
-                    next_task = Task_2(status='PENDING')
-                    self.flow.add_task(next_task, precursor=self)
-                    self.status = 'COMPLETED'
-        task_classes.append(Task_1)
+            @classmethod
+            def tick_task(cls, *args, task=None, ctx=None, **kwargs):
+                super().tick_task(*args, task=task, **kwargs)
+                if cls.allow_completion:
+                    next_task = {'task_engine': TaskEngine_2.__name__,
+                                 'status': 'PENDING'}
+                    ctx['flow'].add_task(next_task,
+                                         precursor_keys=[task['key']])
+                    task['status'] = 'COMPLETED'
+        task_engines.append(TaskEngine_1)
 
-        class Task_2(BaseTestTask):
+        class TaskEngine_2(BaseTaskEngine):
             allow_completion = False
 
-            def tick(self, *args, **kwargs):
-                super().tick()
-                if self.__class__.allow_completion:
-                    self.status = 'COMPLETED'
-        task_classes.append(Task_2)
+            @classmethod
+            def tick_task(cls, *args, task=None, ctx=None, **kwargs):
+                super().tick_task(*args, task=task, **kwargs)
+                if cls.allow_completion: task['status'] = 'COMPLETED'
+        task_engines.append(TaskEngine_2)
 
-        keyed_task_classes = {task_class.__name__: task_class
-                              for task_class in task_classes}
-        return keyed_task_classes
+        keyed_task_engines = {task_engine.__name__: task_engine
+                              for task_engine in task_engines}
+        return keyed_task_engines
 
-    def generate_flow_engine(self, keyed_task_classes=None):
+    def generate_flow_engine(self, keyed_task_engines=None):
         flow_engine = FlowEngine()
-        for task_class in keyed_task_classes.values():
-            flow_engine.register_task_class(task_class=task_class)
+        for task_engine in keyed_task_engines.values():
+            flow_engine.register_task_engine(task_engine=task_engine)
         return flow_engine
 
-    def generate_flow_runner(self, flow_engine=None,
-                                 flow_client=None):
+    def generate_flow_runner(self, flow_engine=None, flow_client=None):
         flow_runner = BaseFlowRunner(flow_engine=flow_engine,
-                                             flow_client=flow_client)
+                                     flow_client=flow_client)
         return flow_runner
 
     def populate_flows(self):
         flow = Flow()
-        root_task = self.keyed_task_classes['Task_0'](flow=flow,
-                                                      status='PENDING')
+        root_task = {'task_engine': 'TaskEngine_0', 'status': 'PENDING'}
         flow.add_task(task=root_task, as_root=True)
         serialization = self.flow_engine.serialize_flow(flow)
         flow_model = FlowModel.objects.create(
@@ -101,48 +105,37 @@ class FlowRunnerE2ETestCase(TestCase):
         states.append(self.get_state(prev_state=None))
 
         def tick_and_capture_state(label=None):
-            #print("tick_and_capture", label)
             self.flow_runner.tick()
             states.append(self.get_state(prev_state=states[-1]))
 
         tick_and_capture_state(label='a')
-        self.assertEqual(
-            states[-1]['task_classes']['Task_0']['ticks'], 1)
+        self.assertEqual(states[-1]['task_engines']['TaskEngine_0']['ticks'], 1)
 
         tick_and_capture_state(label='b')
-        self.assertEqual(
-            states[-1]['task_classes']['Task_0']['ticks'], 1)
-        self.assertEqual(
-            states[-1]['task_classes']['Task_1']['ticks'], 1)
+        self.assertEqual(states[-1]['task_engines']['TaskEngine_0']['ticks'], 1)
+        self.assertEqual(states[-1]['task_engines']['TaskEngine_1']['ticks'], 1)
 
         tick_and_capture_state(label='c')
-        self.assertEqual(
-            states[-1]['task_classes']['Task_1']['ticks'], 2)
-        self.assertFalse(
-            'ticks' in states[-1]['task_classes']['Task_2'])
+        self.assertEqual(states[-1]['task_engines']['TaskEngine_1']['ticks'], 2)
+        self.assertFalse('ticks' in states[-1]['task_engines']['TaskEngine_2'])
 
-        self.keyed_task_classes['Task_1'].allow_completion = True
+        self.keyed_task_engines['TaskEngine_1'].allow_completion = True
         tick_and_capture_state(label='d')
-        self.assertEqual(
-            states[-1]['task_classes']['Task_1']['ticks'], 3)
+        self.assertEqual(states[-1]['task_engines']['TaskEngine_1']['ticks'], 3)
 
         tick_and_capture_state(label='e')
-        self.assertEqual(
-            states[-1]['task_classes']['Task_1']['ticks'], 3)
-        self.assertEqual(
-            states[-1]['task_classes']['Task_2']['ticks'], 1)
+        self.assertEqual(states[-1]['task_engines']['TaskEngine_1']['ticks'], 3)
+        self.assertEqual(states[-1]['task_engines']['TaskEngine_2']['ticks'], 1)
 
-        self.keyed_task_classes['Task_2'].allow_completion = True
+        self.keyed_task_engines['TaskEngine_2'].allow_completion = True
         tick_and_capture_state(label='f')
-        self.assertEqual(
-            states[-1]['task_classes']['Task_2']['ticks'], 2)
+        self.assertEqual(states[-1]['task_engines']['TaskEngine_2']['ticks'], 2)
         self.assertEqual(
             states[-1]['flows'][self.flow_uuid]['status'], 'COMPLETED')
 
         tick_and_capture_state(label='g')
-        self.assertEqual(
-            states[-2]['flows'][self.flow_uuid]['modified'],
-            states[-1]['flows'][self.flow_uuid]['modified'])
+        self.assertEqual(states[-2]['flows'][self.flow_uuid]['modified'],
+                         states[-1]['flows'][self.flow_uuid]['modified'])
 
     def get_state(self, prev_state=None):
         state = {
@@ -150,9 +143,9 @@ class FlowRunnerE2ETestCase(TestCase):
                 flow_model.uuid: flow_model.__dict__
                 for flow_model in FlowModel.objects.all()
             },
-            'task_classes': {
-                key: task_class.__dict__
-                for key, task_class in self.keyed_task_classes.items()
+            'task_engines': {
+                key: task_engine.__dict__
+                for key, task_engine in self.keyed_task_engines.items()
             }
         }
         return state
