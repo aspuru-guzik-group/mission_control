2b7e88dbac6276a20c90358a544e46c6dbc16ace
adorsk <adorsk@fas.harvard.edu>
2017-02-02 09:39:56 -0500

    change to use ctx.transform_value, instead of render template

--- mc/dj/job_runners/action_processor.py
+++ mc/dj/job_runners/action_processor.py
@@ -21,12 +21,8 @@ class ActionProcessor(object):
         self.set_ctx_value(ctx=ctx, **params)
 
     def set_ctx_value(self, ctx=None, target=None, value=None):
-        if self.is_template_value(value):
-            value = ctx.render_template(value['template'])
+        value = ctx.transform_value(value=value)
         ctx.set(target=target, value=value)
-
-    def is_template_value(self, value=None):
-        return isinstance(value, collections.Mapping) and ('template' in value)
 
     def process_action(self, action=None, ctx=None):
         handler = self.get_handler_for_action(action=action)
@@ -71,5 +67,13 @@ class ActionCtx(object):
             cursor = next_cursor
         cursor[path_elements[-1]] = value
 
+    def transform_value(self, value=None):
+        if self.is_template_value(value):
+            value = self.render_template(value['template'])
+        return value
+
     def render_template(self, template=None):
         return jinja2.Template(template).render(ctx=self.ctx)
+
+    def is_template_value(self, value=None):
+        return isinstance(value, collections.Mapping) and ('template' in value)
