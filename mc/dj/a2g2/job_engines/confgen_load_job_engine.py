bd1491226f08808413c23138240c3b81a99d40e9
adorsk <adorsk@fas.harvard.edu>
2017-02-03 15:59:01 -0500

    add logic to parse raw data to chemthing_dict

--- mc/dj/a2g2/job_engines/confgen_load_job_engine.py
+++ mc/dj/a2g2/job_engines/confgen_load_job_engine.py
@@ -1,3 +1,51 @@
+import glob
+import os
+
+import pybel
+
+
 class ConfgenLoadJobEngine(object):
-    pass
+    def parse_job_dir(self, job_dir=None, includes=None, excludes=None):
+        paths_to_parse = self.get_paths_to_parse(job_dir=job_dir,
+                                                 includes=includes,
+                                                 excludes=excludes)
+        chemthing_dicts = []
+        for path in paths_to_parse: chemthing_dicts.extend(self.parse_path(path))
+        return chemthing_dicts
+
+    def get_paths_to_parse(self, job_dir=None, includes=None, excludes=None):
+        if not includes: includes = ['raw_data/**']
+        paths_to_include = self.get_paths_for_globs(root_dir=job_dir,
+                                                    globs=includes)
+        paths_to_exclude = self.get_paths_for_globs(root_dir=job_dir,
+                                                    globs=excludes)
+        paths_to_parse = set(paths_to_include).difference(set(paths_to_exclude))
+        return paths_to_parse
+
+    def get_paths_for_globs(self, root_dir=None, globs=None):
+        paths = []
+        globs = globs or []
+        for _glob in globs:
+            full_glob = os.path.join(root_dir, _glob)
+            all_paths = glob.glob(full_glob, recursive=True)
+            file_paths = [path for path in all_paths if os.path.isfile(path)]
+            paths.extend(file_paths)
+        return paths
+
+    def parse_path(self, path=None):
+        chemthing_dicts = []
+        pb_mols = self.file_to_pb_mols(path=path)
+        for i, pb_mol in enumerate(pb_mols):
+            chemthing_dict = self.pb_mol_to_chemthing_dict(pb_mol)
+            chemthing_dict['props']['mol_idx'] = i
+            chemthing_dicts.append(chemthing_dict)
+        return chemthing_dicts
+
+    def file_to_pb_mols(self, path=None):
+        ext = os.path.splitext(path)[1].lstrip('.')
+        return pybel.readfile(ext, path)
+
+    def pb_mol_to_chemthing_dict(self, pb_mol=None):
+        chemthing_dict = {'cml': pb_mol.write('cml'), 'props': {}}
+        return chemthing_dict
 
--- mc/dj/a2g2/job_engines/tests/test_confgen_load_job_engine.py
+++ mc/dj/a2g2/job_engines/tests/test_confgen_load_job_engine.py
@@ -3,7 +3,9 @@ import tempfile
 import unittest
 from unittest.mock import call, Mock
 
-from rdkit import AllChem, Chem
+import pybel
+from rdkit import Chem
+from rdkit.Chem import AllChem
 
 from ..confgen_load_job_engine import ConfgenLoadJobEngine
 
@@ -11,35 +13,10 @@ from ..confgen_load_job_engine import ConfgenLoadJobEngine
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.engine = ConfgenLoadJobEngine()
-        self.conformers = self.generate_conformers()
-
-    def generate_conformers(self):
-        mol = Chem.AddHs(Chem.MolFromSmiles('Cc1ccccc1'))
-        conformer_ids = AllChem.EmbedMultipleConfs(mol, numConfs=10)
-        conformers = mol.GetConformers()
 
 class RunJobDirTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.job_dir = self.generate_job_dir()
-        self.job_spec = self.generate_job_spec()
-
-    def generate_job_dir(self):
-        job_dir = tempfile.mkdtemp()
-        raw_data_dir = self.generate_raw_data_dir(parent_dir=job_dir)
-        for i, conformer in enumerate(self.conformers):
-            fmt = 'pdb'
-            conformer_file_name = 'conformer_%s.%s' % (i, fmt)
-            conformer_file_path = os.path.join(raw_data_dir,
-                                               conformer_file_name)
-            self.output_conformer(conformer=conformer,
-                                  format='pdb',
-                                  output_file_path=conformer_file_path)
-        return job_dir
-
-    def generate_raw_data_dir(self, parent_dir=None):
-        raw_data_dir = os.path.join(parent_dir, 'raw_data')
-        os.makedirs(raw_data_dir)
 
     def test_uploads_parse_results(self):
         self.engine.parse_inputs = Mock()
@@ -52,12 +29,65 @@ class RunJobDirTestCase(BaseTestCase):
         self.assertEqual(self.engine.upload_objects.call_args,
                          call(objects=self.engine.parse_inputs.return_value))
 
-class ParseInputsTestCase(BaseTestCase):
-    def test_generates_objects_from_inputs(self):
-        self.fail()
+class ParseJobDirTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.mol = self.generate_mol_w_conformers()
+        self.job_dir = self.generate_job_dir()
+
+    def generate_mol_w_conformers(self):
+        mol = Chem.AddHs(Chem.MolFromSmiles('Cc1ccccc1'))
+        AllChem.EmbedMultipleConfs(mol, numConfs=3)
+        return mol
+
+    def generate_job_dir(self):
+        job_dir = tempfile.mkdtemp()
+        self.generate_raw_data_dir(parent_dir=job_dir)
+        return job_dir
+
+    def generate_raw_data_dir(self, parent_dir=None):
+        raw_data_dir = os.path.join(parent_dir, 'raw_data')
+        os.makedirs(raw_data_dir)
+        conformer_file_path = os.path.join(raw_data_dir, 'conformers.sdf')
+        with open(conformer_file_path, 'w') as conformer_file:
+            writer = Chem.SDWriter(conformer_file)
+            for conformer in self.mol.GetConformers():
+                writer.write(self.mol, confId=conformer.GetId())
+            writer.close()
+
+    def test_generates_chemthings_from_inputs(self):
+        chemthings = self.engine.parse_job_dir(self.job_dir,
+                                               includes=['raw_data/*'])
+        expected_chemthings = self.generate_chemthings_for_mol(mol=self.mol)
+        self.assertEqual(self.get_sorted_chemthings(chemthings),
+                         self.get_sorted_chemthings(expected_chemthings))
+
+    def generate_chemthings_for_mol(self, mol=None):
+        chemthings = [
+            self.generate_chemthing_for_conformer(conformer,
+                                                  props={'mol_idx': i})
+            for i, conformer in enumerate(mol.GetConformers())
+        ]
+        return chemthings
+
+    def generate_chemthing_for_conformer(self, conformer=None, props=None):
+        chemthing = {
+            'cml': self.generate_cml_for_conformer(conformer),
+            'props': props,
+        }
+        return chemthing
+
+    def generate_cml_for_conformer(self, conformer=None):
+        serialized_conformer = Chem.MolToMolBlock(conformer.GetOwningMol(),
+                                                  confId=conformer.GetId())
+        pb_mol = pybel.readstring('sdf', serialized_conformer)
+        return pb_mol.write('cml')
+
+    def get_sorted_chemthings(self, chemthings):
+        return sorted(chemthings, key=lambda cthing: cthing['props']['mol_idx'])
 
 class UploadTestCase(BaseTestCase):
-    def test_makes_expected_uploads(self):
+    def test_uploads_chemthings(self):
         self.fail()
 
     def test_outputs_upload_results(self):
