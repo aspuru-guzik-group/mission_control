32f0d03fcfc5930e69de7802f14f5b321b09899e
adorsk <adorsk@fas.harvard.edu>
2017-03-23 15:02:42 -0400

    changing format of flow spec

--- mc/a2g2/flow_generators/compute_parse_load_flow_generator.py
+++ mc/a2g2/flow_generators/compute_parse_load_flow_generator.py
@@ -1,3 +1,5 @@
+import yaml
+
 from mc.flow_engines.flow import Flow
 from mc.a2g2.node_engines.job_node_engine import JobNodeEngine
 
@@ -21,136 +23,137 @@ class ComputeParseLoadFlowGenerator(base_flow_generator.BaseFlowGenerator):
         flow.data['flow_spec'] = flow_spec
         flow.add_node(
             as_root=True,
-            key='compute_node',
+            key='compute',
             node=cls.generate_compute_node(flow=flow)
         )
         flow.add_node(
-            key='parse_node',
-            precursor_keys=['compute_node'],
+            key='parse',
+            precursor_keys=['compute'],
             node=cls.generate_parse_node(flow=flow)
         )
         flow.add_node(
-            key='load_node',
-            precursor_keys=['parse_node'],
+            key='load',
+            precursor_keys=['parse'],
             node=cls.generate_load_node(flow=flow)
         )
         return flow
 
     @classmethod
     def generate_compute_node(cls, flow=None):
-        node = {
-            'node_tasks': [
-                {
-                    'task_type': 'run_job',
-                    'task_params': {
-                        'job_spec': {
-                            'job_params': flow.data['flow_spec'].get(
-                                'compute_job_params', {}),
-                            'job_tasks': [
-                                {'type': 'job:execute'},
-                                {
-                                    'type': 'a2g2:storage:upload',
-                                    'description': 'upload completed dir',
-                                    'params': {
-                                        'src': '{{job.completed_dir}}'
-                                    }
-                                },
-                            ],
-                        }
-                    }
-                }
-            ]
-        }
+        node = yaml.load(
+            '''
+            node_tasks:
+            - task_key: run_job
+              task_params:
+                job_spec:
+                  job_params: %(job_params_yaml)s
+                  job_tasks:
+                  - task_key: execute
+                    task_type: a2g2.tasks.jobs.execute
+                  - task_key: put_artifact
+                    task_params:
+                      src: '{{ctx.tasks.execute.data.artifact}}'
+                    task_type: a2g2.tasks.jobs.storage.put
+                  - task_key: expose_artifact
+                    task_params:
+                      value_specs:
+                      - dest: ctx.job.data.outputs.artifact
+                        value: '{{ctx.tasks.put_artifact.data.artifact}}'
+                    task_type: a2g2.tasks.set_values
+              task_type: a2g2.tasks.nodes.run_job
+            - task_key: expose_job_outputs
+              task_params:
+                value_specs:
+                - dest: ctx.node.data.outputs
+                  value: '{{ctx.tasks.run_job.data.outputs}}'
+              task_type: a2g2.tasks.set_values
+            ''' % {
+                'job_params_yaml': cls.dump_inline_yaml(
+                    flow.data['flow_spec'].get('compute_job_params', {}))
+            }
+        )
         return node
 
     @classmethod
+    def dump_inline_yaml(cls, obj=None):
+        return yaml.dump(obj, default_style='"', default_flow_style=True)\
+                .strip()
+
+    @classmethod
     def generate_parse_node(cls, flow=None):
-        node = {
-            'node_tasks': [
-                {
-                    'task_type': 'mc:set_values',
-                    'description': 'storage_meta plumbing', 
-                    'task_params': {
-                        'value_specs': [
-                            {
-                                'target': 'node.data.storage_meta_json',
-                                'src': (
-                                    '{{flow.nodes.%s.output.storage_meta}}' % (
-                                        'compute_node'
-                                    )
-                                )
-                            }
-                        ]
-                    },
-                },
-                {
-                    'task_type': 'run_job',
-                    'task_params': {
-                        'job_spec': {
-                            'job_params': flow.data['flow_spec'].get(
-                                'parse_job_params'),
-                            'job_tasks': [
-                                {
-                                    'task_type': 'a2g2:storage:download',
-                                    'task_params': {
-                                        'storage_meta_json': '<UNSET>',
-                                        'dest': '{{ctx.job_dir}}/dir_to_parse'
-                                    }
-                                },
-                                {'task_type': 'job:execute'},
-                                {
-                                    'task_type': 'a2g2:storage:upload',
-                                    'task_params': {
-                                        'src': '{{job.completed_dir}}'
-                                    }
-                                },
-                            ]
-                        }
-                    }
-                }
-            ]
-        }
+        node = yaml.load(
+            '''
+            node_tasks:
+            - task_key: set_job_input_artifact
+              task_type: a2g2.tasks.set_values
+              task_params:
+                value_specs:
+                  - dest: ctx.node.tasks.run_job.task_params.job_spec
+                      .job_tasks.0.task_params.artifact
+                    value: '{{ctx.flow.nodes.compute.data.outputs.artifact}}'
+            - task_key: run_job
+              task_params:
+                job_spec:
+                  job_params: %(job_params_yaml)s
+                  job_tasks:
+                  - task_key: get_input_artifact
+                    task_type: a2g2.tasks.jobs.storage.get
+                    task_params:
+                      artifact: TO_BE_WIRED_IN
+                      dest: '{{ctx.job.dir}}/dir_to_parse'
+                  - task_key: execute
+                    task_type: a2g2.tasks.jobs.execute
+                  - task_key: put_artifact
+                    task_params:
+                      src: '{{ctx.tasks.execute.data.artifact}}'
+                    task_type: a2g2.tasks.jobs.storage.put
+                  - task_key: expose_artifact
+                    task_params:
+                      value_specs:
+                      - dest: ctx.job.data.outputs.artifact
+                        value: '{{ctx.tasks.put_artifact.data.artifact}}'
+                    task_type: a2g2.tasks.set_values
+              task_type: a2g2.tasks.nodes.run_job
+            - task_key: expose_job_outputs
+              task_params:
+                value_specs:
+                - dest: ctx.node.data.outputs
+                  value: '{{ctx.tasks.run_job.data.outputs}}'
+              task_type: a2g2.tasks.set_values
+            ''' % {
+                'job_params_yaml': cls.dump_inline_yaml(
+                    flow.data['flow_spec'].get('parse_job_params', {}))
+            }
+        )
         return node
 
     @classmethod
     def generate_load_node(cls, flow=None):
-        node = {
-            'node_tasks': [
-                {
-                    'task_type': 'mc:set_values',
-                    'description': 'storage_meta plumbing', 
-                    'task_params': {
-                        'value_specs': [
-                            {
-                                'target': 'node.data.storage_meta_json',
-                                'src': (
-                                    '{{flow.nodes.%s.output.storage_meta}}' % (
-                                        'parse_node'
-                                    )
-                                )
-                            }
-                        ]
-                    },
-                },
-                {
-                    'task_type': 'run_job',
-                    'task_params': {
-                        'job_spec': {
-                            'job_params': flow.data['flow_spec'].get(
-                                'load_job_params'),
-                            'job_tasks': [
-                                {
-                                    'task_type': 'a2g2:storage:download',
-                                    'task_params': {
-                                        'storage_meta_json': '<UNSET>',
-                                        'dest': '{{ctx.job_dir}}/dir_to_load'
-                                    }
-                                },
-                                {'task_type': 'job:execute'}
-                            ]
-                        }
-                    }
-                }
-            ]
-        }
+        node = yaml.load(
+            '''
+            node_tasks:
+            - task_key: set_job_input_artifact
+              task_type: a2g2.tasks.set_values
+              task_params:
+                value_specs:
+                  - dest: ctx.node.tasks.run_job.task_params.job_spec
+                      .job_tasks.0.task_params.artifact
+                    value: '{{ctx.flow.nodes.parse.data.outputs.artifact}}'
+            - task_key: run_job
+              task_params:
+                job_spec:
+                  job_params: %(job_params_yaml)s
+                  job_tasks:
+                  - task_key: get_input_artifact
+                    task_type: a2g2.tasks.jobs.storage.get
+                    task_params:
+                      artifact: TO_BE_WIRED_IN
+                      dest: '{{ctx.job.dir}}/dir_to_load'
+                  - task_key: execute
+                    task_type: a2g2.tasks.jobs.execute
+            ''' % {
+                'job_params_yaml': cls.dump_inline_yaml(
+                    flow.data['flow_spec'].get('load_job_params', {}))
+            }
+        )
         return node
--- mc/a2g2/flow_generators/tests/test_compute_parse_load_flow_generator.py
+++ mc/a2g2/flow_generators/tests/test_compute_parse_load_flow_generator.py
@@ -1,6 +1,7 @@
 from collections import defaultdict
 import unittest
 from unittest.mock import patch, MagicMock
+import yaml
 
 from .. import compute_parse_load_flow_generator
 
@@ -9,6 +10,15 @@ class BaseTestCase(unittest.TestCase):
         self.flow_generator = \
                 compute_parse_load_flow_generator.ComputeParseLoadFlowGenerator
 
+    def generate_flow_w_mock_job_params(self, params_key=None):
+        flow = MagicMock()
+        flow.data = {'flow_spec': {params_key: 'mock_job_params'}}
+        return flow
+
+    def dump_inline_yaml(self, obj=None):
+        return yaml.dump(obj, default_style='"', default_flow_style=True)\
+                .strip()
+
 class GetDependenciesTestCase(BaseTestCase):
     def test_returns_expected_dependencies(self):
         expected_dependencies = {
@@ -16,10 +26,12 @@ class GetDependenciesTestCase(BaseTestCase):
         }
         self.assertEqual(self.flow_generator.get_dependencies(),
                          expected_dependencies)
+    
 
 class GenerateFlowTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
+        self.expected_node_keys = ['compute', 'parse', 'load']
         self.patchers = self.setup_patchers()
         self.mocks = {key: patcher.start()
                       for key, patcher in self.patchers.items()}
@@ -29,51 +41,60 @@ class GenerateFlowTestCase(BaseTestCase):
 
     def setup_patchers(self):
         flow_generator_patches = {}
-        for job_name in ['compute', 'parse', 'load']:
-            attr_name = 'generate_{job_name}_node'.format(job_name=job_name)
-            flow_generator_patches[attr_name] = MagicMock(
-                return_value=defaultdict(MagicMock))
+        for node_key in self.expected_node_keys:
+            attr = 'generate_{node_key}_node'.format(node_key=node_key)
+            flow_generator_patches[attr] = \
+                    MagicMock(return_value=defaultdict(MagicMock))
         patchers = {'flow_generator': patch.multiple(self.flow_generator,
                                                      **flow_generator_patches)}
         return patchers
 
     def test_flow_has_expected_nodes(self):
         flow = self.flow_generator.generate_flow()
-        for job_name in ['compute', 'parse', 'load']:
-            node_name = '{job_name}_node'.format(job_name=job_name)
+        for node_key in self.expected_node_keys:
             self.assertEqual(
-                flow.nodes[node_name],
+                flow.nodes[node_key],
                 getattr(self.flow_generator,
-                        'generate_%s' % node_name).return_value
+                        'generate_%s_node' % node_key).return_value
             )
-        self.assertEqual(flow.root_node_key, 'compute_node')
+        self.assertEqual(flow.root_node_key, 'compute')
 
 class GenerateComputeNodeTestCase(BaseTestCase):
     def test_generates_expected_compute_node(self):
-        flow = MagicMock()
-        expected_node = {
-            'node_tasks': [
-                {
-                    'task_type': 'run_job',
-                    'task_params': {
-                        'job_spec': {
-                            'job_params': flow.data['flow_spec'].get(
-                                'compute_job_params', {}),
-                            'job_tasks': [
-                                {'type': 'job:execute'},
-                                {
-                                    'type': 'a2g2:storage:upload',
-                                    'description': 'upload completed dir',
-                                    'params': {
-                                        'src': '{{job.completed_dir}}'
-                                    }
-                                },
-                            ],
-                        }
-                    }
-                }
-            ]
-        }
+        flow = self.generate_flow_w_mock_job_params(
+            params_key='compute_job_params')
+        expected_node = yaml.load(
+            '''
+            node_tasks:
+            - task_key: run_job
+              task_params:
+                job_spec:
+                  job_params: %(job_params_yaml)s
+                  job_tasks:
+                  - task_key: execute
+                    task_type: a2g2.tasks.jobs.execute
+                  - task_key: put_artifact
+                    task_params:
+                      src: '{{ctx.tasks.execute.data.artifact}}'
+                    task_type: a2g2.tasks.jobs.storage.put
+                  - task_key: expose_artifact
+                    task_params:
+                      value_specs:
+                      - dest: ctx.job.data.outputs.artifact
+                        value: '{{ctx.tasks.put_artifact.data.artifact}}'
+                    task_type: a2g2.tasks.set_values
+              task_type: a2g2.tasks.nodes.run_job
+            - task_key: expose_job_outputs
+              task_params:
+                value_specs:
+                - dest: ctx.node.data.outputs
+                  value: '{{ctx.tasks.run_job.data.outputs}}'
+              task_type: a2g2.tasks.set_values
+            ''' % {
+                'job_params_yaml': self.dump_inline_yaml(
+                    obj=flow.data['flow_spec'].get('compute_job_params', {}))
+            }
+        )
         self.assertEqual(
             self.flow_generator.generate_compute_node(flow=flow),
             expected_node
@@ -81,52 +102,52 @@ class GenerateComputeNodeTestCase(BaseTestCase):
 
 class GenerateParseNode(BaseTestCase):
     def test_generates_expected_parse_node(self):
-        flow = MagicMock()
-        expected_node = {
-            'node_tasks': [
-                {
-                    'task_type': 'mc:set_values',
-                    'description': 'storage_meta plumbing',
-                    'task_params': {
-                        'value_specs': [
-                            {
-                                'target': 'node.data.storage_meta_json',
-                                'src': (
-                                    '{{flow.nodes.%s.output.storage_meta}}' % (
-                                        'compute_node'
-                                    )
-                                )
-                            }
-                        ]
-                    },
-                },
-                {
-                    'task_type': 'run_job',
-                    'task_params': {
-                        'job_spec': {
-                            'job_params': flow.data['flow_spec'].get(
-                                'parse_job_params'),
-                            'job_tasks': [
-                                {
-                                    'task_type': 'a2g2:storage:download',
-                                    'task_params': {
-                                        'storage_meta_json': '<UNSET>',
-                                        'dest': '{{ctx.job_dir}}/dir_to_parse'
-                                    }
-                                },
-                                {'task_type': 'job:execute'},
-                                {
-                                    'task_type': 'a2g2:storage:upload',
-                                    'task_params': {
-                                        'src': '{{job.completed_dir}}'
-                                    }
-                                },
-                            ]
-                        }
-                    }
-                }
-            ]
-        }
+        flow = self.generate_flow_w_mock_job_params(
+            params_key='parse_job_params')
+        expected_node = yaml.load(
+            '''
+            node_tasks:
+            - task_key: set_job_input_artifact
+              task_type: a2g2.tasks.set_values
+              task_params:
+                value_specs:
+                  - dest: ctx.node.tasks.run_job.task_params.job_spec
+                      .job_tasks.0.task_params.artifact
+                    value: '{{ctx.flow.nodes.compute.data.outputs.artifact}}'
+            - task_key: run_job
+              task_params:
+                job_spec:
+                  job_params: %(job_params_yaml)s
+                  job_tasks:
+                  - task_key: get_input_artifact
+                    task_type: a2g2.tasks.jobs.storage.get
+                    task_params:
+                      artifact: TO_BE_WIRED_IN
+                      dest: '{{ctx.job.dir}}/dir_to_parse'
+                  - task_key: execute
+                    task_type: a2g2.tasks.jobs.execute
+                  - task_key: put_artifact
+                    task_params:
+                      src: '{{ctx.tasks.execute.data.artifact}}'
+                    task_type: a2g2.tasks.jobs.storage.put
+                  - task_key: expose_artifact
+                    task_params:
+                      value_specs:
+                      - dest: ctx.job.data.outputs.artifact
+                        value: '{{ctx.tasks.put_artifact.data.artifact}}'
+                    task_type: a2g2.tasks.set_values
+              task_type: a2g2.tasks.nodes.run_job
+            - task_key: expose_job_outputs
+              task_params:
+                value_specs:
+                - dest: ctx.node.data.outputs
+                  value: '{{ctx.tasks.run_job.data.outputs}}'
+              task_type: a2g2.tasks.set_values
+            ''' % {
+                'job_params_yaml': self.dump_inline_yaml(
+                    obj=flow.data['flow_spec'].get('parse_job_params', {}))
+            }
+        )
         self.assertEqual(
             self.flow_generator.generate_parse_node(flow=flow),
             expected_node
@@ -134,46 +155,35 @@ class GenerateParseNode(BaseTestCase):
 
 class GenerateLoadNodeTestCase(BaseTestCase):
     def test_generates_expected_load_node(self):
-        flow = MagicMock()
-        expected_node = {
-            'node_tasks': [
-                {
-                    'task_type': 'mc:set_values',
-                    'description': 'storage_meta plumbing',
-                    'task_params': {
-                        'value_specs': [
-                            {
-                                'target': 'node.data.storage_meta_json',
-                                'src': (
-                                    '{{flow.nodes.%s.output.storage_meta}}' % (
-                                        'parse_node'
-                                    )
-                                )
-                            }
-                        ]
-                    },
-                },
-                {
-                    'task_type': 'run_job',
-                    'task_params': {
-                        'job_spec': {
-                            'job_params': flow.data['flow_spec'].get(
-                                'load_job_params'),
-                            'job_tasks': [
-                                {
-                                    'task_type': 'a2g2:storage:download',
-                                    'task_params': {
-                                        'storage_meta_json': '<UNSET>',
-                                        'dest': '{{ctx.job_dir}}/dir_to_load'
-                                    }
-                                },
-                                {'task_type': 'job:execute'},
-                            ]
-                        }
-                    }
-                }
-            ]
-        }
+        flow = self.generate_flow_w_mock_job_params(
+            params_key='load_job_params')
+        expected_node = yaml.load(
+            '''
+            node_tasks:
+            - task_key: set_job_input_artifact
+              task_type: a2g2.tasks.set_values
+              task_params:
+                value_specs:
+                  - dest: ctx.node.tasks.run_job.task_params.job_spec
+                      .job_tasks.0.task_params.artifact
+                    value: '{{ctx.flow.nodes.parse.data.outputs.artifact}}'
+            - task_key: run_job
+              task_params:
+                job_spec:
+                  job_params: %(job_params_yaml)s
+                  job_tasks:
+                  - task_key: get_input_artifact
+                    task_type: a2g2.tasks.jobs.storage.get
+                    task_params:
+                      artifact: TO_BE_WIRED_IN
+                      dest: '{{ctx.job.dir}}/dir_to_load'
+                  - task_key: execute
+                    task_type: a2g2.tasks.jobs.execute
+            ''' % {
+                'job_params_yaml': self.dump_inline_yaml(
+                    obj=flow.data['flow_spec'].get('load_job_params', {}))
+            }
+        )
         self.assertEqual(
             self.flow_generator.generate_load_node(flow=flow),
             expected_node
