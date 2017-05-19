5df4dc26154b4529b89b86734acde7b1bf0fc04f
adorsk <adorsk@fas.harvard.edu>
2017-05-19 10:27:34 -0400

    partway through dj dao

--- mc/mc_client/dj_dao.py
+++ mc/mc_client/dj_dao.py
@@ -1,22 +1,83 @@
+import contextlib
+import importlib
 import logging
-
-from mc.missions.utils import queue_utils as _queue_utils
 
 
 class DjDao(object):
-    def __init__(self, models=None, serializers=None, queue_utils=None,
-                 logger=None):
-        self.models = models
-        self.serializers = serializers
-        self.queue_utils = queue_utils or _queue_utils
+    DEFAULT_DB_CFG = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': ':memory:'
+        }
+    }
+    MC_APP = 'mc.missions'
+    INSTALLED_APPS_CFG = [MC_APP]
+
+    def __init__(self, db_cfg=None, mc_modules=None, logger=None):
         self.logger = logger or logging
+        self.db_cfg = db_cfg or self.DEFAULT_DB_CFG
+        self._mc_modules = mc_modules
+
+    @contextlib.contextmanager
+    def get_mc_modules_ctx(self):
+        """This is a context manager that sets self.mc_modules to either
+        the mc_modules provided to __init__, or modules loaded from
+        the mc django app, in a django context"""
+        if hasattr(self, 'mc_modules'): yield
+        elif self._mc_modules is not None:
+            self.mc_modules = self._mc_modules
+            yield
+        else:
+            with self.get_dj_ctx():
+                self.mc_modules = {
+                    module_name: self.import_mc_module(module_name)
+                    for module_name in ['models', 'serializers']
+                }
+                self.mc_modules['queue_utils'] = self.import_mc_module(
+                    'utils.queue_utils')
+                yield
+        if hasattr(self, 'mc_modules'): del self.mc_modules
+
+    def import_mc_module(self, module_name=None):
+        prefixed_module_name = '{mc_app}.{module_name}'.format(
+            mc_app=self.MC_APP, module_name=module_name)
+        return importlib.import_module(prefixed_module_name)
+
+    def get_dj_ctx(self):
+        """ This is some hackiness to setup a temporary django environment.
+        It patches django internals just enough to call django.setup().
+        """
+        from unittest.mock import patch
+        import sys
+        from django.apps import registry as _dj_apps_registry
+        from django.conf import LazySettings
+        with patch.dict(sys.modules, {_dj_apps_registry.__name__: None}):
+            _apps = _dj_apps_registry.Apps(installed_apps=None)
+        _settings = LazySettings()
+        exit_stack = contextlib.ExitStack()
+        for ctx_mgr in [
+            patch('django.conf.settings', new=_settings),
+            patch('django.apps.apps', new=_apps)
+        ]: exit_stack.enter_context(ctx_mgr)
+        from django.conf import settings
+        settings.configure(
+            DATABASES=self.db_cfg,
+            INSTALLED_APPS=self.INSTALLED_APPS_CFG
+        )
+        import django
+        django.setup(set_prefix=False)
+        return exit_stack
 
     def create_flow(self, flow_kwargs=None):
-        flow_model = self.models['Flow'].objects.create(**flow_kwargs)
-        return self.serialize_flow_model(flow_model=flow_model)
+        with self.get_mc_modules_ctx():
+            flow_model = self.mc_modules['models'].Flow.objects.create(
+                **flow_kwargs)
+            return self.serialize_flow_model(flow_model=flow_model)
 
     def serialize_flow_model(self, flow_model=None):
-        return self.serializers['Flow'](flow_model).data
+        with self.get_mc_modules_ctx():
+            return self.mc_modules['serializers'].FlowSerializer(
+                flow_model).data
 
     def get_flows(self, query=None):
         flow_models = self.get_flow_models(query=query)
@@ -30,9 +91,10 @@ class DjDao(object):
         return flow_models
  
     def flow_query_to_queryset(self, query=None):
-        if query: self.validate_query(query=query)
-        qs = self.models['Flow'].objects
-        return qs.filter(**self.get_dj_filter_kwargs_for_query(query=query))
+        with self.get_mc_modules_ctx():
+            if query: self.validate_query(query=query)
+            qs = self.mc_modules['models'].Flow.objects
+            return qs.filter(**self.get_dj_filter_kwargs_for_query(query=query))
 
     def validate_query(self, query=None):
         for _filter in query.get('filters' or []):
@@ -63,22 +125,25 @@ class DjDao(object):
         return dj_filter_kwargs
 
     def patch_flow(self, key=None, patches=None):
-        flow_model = self.models['Flow'].objects.get(uuid=key)
-        self.patch_model(model=flow_model, patches=patches)
-        return self.serialize_flow_model(flow_model=flow_model)
+        with self.get_mc_modules_ctx():
+            flow_model = self.mc_modules['models'].Flow.objects.get(uuid=key)
+            self.patch_model(model=flow_model, patches=patches)
+            return self.serialize_flow_model(flow_model=flow_model)
 
     def patch_model(self, model=None, patches=None):
         patches = patches or {}
-        for k, v in patches.items():
-            setattr(model, k, v)
+        for k, v in patches.items(): setattr(model, k, v)
         model.save()
 
     def create_job(self, job_kwargs=None):
-        job_model = self.models['Job'].objects.create(**job_kwargs)
-        return self.serialize_job_model(job_model=job_model)
+        with self.get_mc_modules_ctx():
+            job_model = self.mc_modules['models'].Job.objects.create(
+                **job_kwargs)
+            return self.serialize_job_model(job_model=job_model)
 
     def serialize_job_model(self, job_model=None):
-        return self.serializers['Job'](job_model).data
+        with self.get_mc_modules_ctx():
+            return self.mc_modules['serializers'].JobSerializer(job_model).data
 
     def get_jobs(self, query=None):
         job_models = self.get_job_models(query=query)
@@ -92,32 +157,43 @@ class DjDao(object):
         return job_models
  
     def job_query_to_queryset(self, query=None):
-        if query: self.validate_query(query=query)
-        qs = self.models['Job'].objects
-        return qs.filter(**self.get_dj_filter_kwargs_for_query(query=query))
+        with self.get_mc_modules_ctx():
+            if query: self.validate_query(query=query)
+            qs = self.mc_modules['models'].Job.objects
+            return qs.filter(**self.get_dj_filter_kwargs_for_query(query=query))
 
     def patch_job(self, key=None, patches=None):
-        job_model = self.models['Job'].objects.get(uuid=key)
-        self.patch_model(model=job_model, patches=patches)
-        return self.serialize_job_model(job_model=job_model)
+        with self.get_mc_modules_ctx():
+            job_model = self.mc_modules['models'].Job.objects.get(uuid=key)
+            self.patch_model(model=job_model, patches=patches)
+            return self.serialize_job_model(job_model=job_model)
 
     def create_queue(self, queue_kwargs=None):
-        queue_model = self.models['Queue'].objects.create(**queue_kwargs)
-        return self.serialize_queue_model(queue_model=queue_model)
+        with self.get_mc_modules_ctx():
+            queue_model = self.mc_modules['models'].Queue.objects.create(
+                **queue_kwargs)
+            return self.serialize_queue_model(queue_model=queue_model)
 
     def serialize_queue_model(self, queue_model=None):
-        return self.serializers['Queue'](queue_model).data
-
-    def flush_mc_db(self):
-        for model_name in ['Job', 'Flow', 'Queue']:
-            self.models[model_name].objects.all().delete()
+        with self.get_mc_modules_ctx():
+            return self.mc_modules['serializers'].QueueSerializer(
+                queue_model).data
 
     def claim_queue_items(self, queue_key=None, params=None):
-        queue_model = self.models['Queue'].objects.get(uuid=queue_key)
-        claimed_item_models = self.queue_utils.claim_queue_items(
-            queue=queue_model, models=self.models)
-        return {
-            'items': self.queue_utils.serialize_queue_items(
-                queue=queue_model, items=claimed_item_models,
-                serializers=self.serializers)
-        }
+        with self.get_mc_modules_ctx():
+            queue_model = self.mc_modules['models'].Queue.objects.get(
+                uuid=queue_key)
+            queue_utils = self.mc_modules['queue_utils']
+            claimed_item_models = queue_utils.claim_queue_items(
+                queue=queue_model, models=self.mc_modules['models'])
+            return {
+                'items': queue_utils.serialize_queue_items(
+                    queue=queue_model, items=claimed_item_models,
+                    serializers=self.mc_modules['serializers'])
+            }
+
+    def flush_mc_db(self):
+        with self.get_mc_modules_ctx():
+            for model_name in ['Job', 'Flow', 'Queue']:
+                ModelCls = getattr(self.mc_modules['models'], model_name)
+                ModelCls.objects.all().delete()
