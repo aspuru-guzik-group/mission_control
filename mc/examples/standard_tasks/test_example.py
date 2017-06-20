2930fc42c04bac90941140189f24d37f0c5298b8
adorsk <adorsk@fas.harvard.edu>
2017-06-20 15:28:30 -0400

    stubbing out example test for switch task

--- mc/examples/standard_tasks/test_example.py
+++ mc/examples/standard_tasks/test_example.py
@@ -11,3 +11,6 @@ class TestExample(unittest.TestCase):
         stdout = subprocess.check_output(cmd, shell=True).decode()
         expected_stdout = 'message set by task_1\n'
         self.assertEqual(stdout, expected_stdout)
+
+    def test_add_test_for_switch_task(self):
+        self.fail()
