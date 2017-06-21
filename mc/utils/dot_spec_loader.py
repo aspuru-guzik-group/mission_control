439a92a4d7f4f81b7130b1bfc892bec75939d229
adorsk <adorsk@fas.harvard.edu>
2017-06-21 10:01:30 -0400

    add logic for callables, dictviews, and tests for dot_spec_loader

--- mc/utils/dot_spec_loader.py
+++ mc/utils/dot_spec_loader.py
@@ -4,7 +4,10 @@ import importlib
 
 class DotSpecLoader(object):
     @classmethod
-    def load_from_dot_spec(self, dot_spec=None):
+    def load_from_dot_spec(cls, dot_spec=None):
+        return cls()._load_from_dot_spec(dot_spec=dot_spec)
+    
+    def _load_from_dot_spec(self, dot_spec=None):
         dot_spec_parts = dot_spec.split(':')
         if len(dot_spec_parts) > 2: raise Exception("invalid dot spec")
         module_name = dot_spec_parts[0]
@@ -12,18 +15,21 @@ class DotSpecLoader(object):
         result = module
         if len(dot_spec_parts) == 2:
             object_dot_spec = dot_spec_parts[1]
-            result = self.get_obj_value_from_dot_spec(obj=module,
-                                                      dot_spec=object_dot_spec)
+            result = self._get_obj_value_from_dot_spec(obj=module,
+                                                       dot_spec=object_dot_spec)
         return result
 
     @classmethod
-    def get_obj_value_from_dot_spec(self, obj=None, dot_spec=None):
+    def get_obj_value_from_dot_spec(cls, obj=None, dot_spec=None):
+        return cls()._get_obj_value_from_dot_spec(obj=obj, dot_spec=dot_spec)
+
+    def _get_obj_value_from_dot_spec(self, obj=None, dot_spec=None):
         path_elements = dot_spec.split('.')
         cursor = obj
         pos = 0
         try:
             for path_element in path_elements:
-                next_cursor = self.get_attr_or_item(obj=cursor, key=path_element)
+                next_cursor = self._get_attr_or_item(obj=cursor, key=path_element)
                 cursor = next_cursor
                 pos += 1
             return cursor
@@ -36,9 +42,16 @@ class DotSpecLoader(object):
                                      partial_path=path_elements[:pos+1]))
 
     @classmethod
-    def get_attr_or_item(self, obj=None, key=None):
+    def get_attr_or_item(cls, obj=None, key=None):
+        return cls()._get_attr_or_item(obj=obj, key=key)
+
+    def _get_attr_or_item(self, obj=None, key=None):
         try:
             if isinstance(obj, collections.abc.Sequence): return obj[int(key)]
-            elif isinstance(obj, collections.abc.Mapping): return obj[key]
-            elif hasattr(obj, key): return getattr(obj, key)
+            if isinstance(obj, collections.abc.Mapping): return obj[key]
+            if isinstance(obj, collections.abc.Mapping): return obj[key]
+            if isinstance(obj, collections.abc.MappingView):
+                return list(obj)[key]
+            if callable(obj): return self._get_attr_or_item(obj(), key)
+            if hasattr(obj, key): return getattr(obj, key)
         except: return None
---
+++ mc/utils/tests/test_dot_spec_loader.py
@@ -0,0 +1,78 @@
+import types
+import unittest
+from unittest.mock import call, MagicMock, patch
+
+from .. import dot_spec_loader
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        self.loader = dot_spec_loader.DotSpecLoader()
+
+class _LoadFromDotSpecTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        importlib_patcher = patch.object(dot_spec_loader, 'importlib')
+        self.addCleanup(importlib_patcher.stop)
+        self.mock_importlib = importlib_patcher.start()
+        self.loader._get_obj_value_from_dot_spec = MagicMock()
+        self.module_part = '1.2.3'
+        self.obj_part = 'a.b.c'
+        self.dot_spec = self.module_part + ':' + self.obj_part
+        self.result = self.loader._load_from_dot_spec(dot_spec=self.dot_spec)
+
+    def test_imports_module(self):
+        self.assertEqual(self.mock_importlib.import_module.call_args,
+                         call(self.module_part))
+
+    def test_gets_obj_value_from_module(self):
+        self.assertEqual(
+            self.loader._get_obj_value_from_dot_spec.call_args,
+            call(obj=self.mock_importlib.import_module.return_value,
+                 dot_spec=self.obj_part)
+        )
+        self.assertEqual(self.result,
+                         self.loader._get_obj_value_from_dot_spec.return_value)
+
+class _GetObjValueFromDotSpecTestCase(BaseTestCase):
+    def test_calls_get_attr_or_item(self):
+        self.loader._get_attr_or_item = MagicMock()
+        obj = MagicMock()
+        path_elements = [str(i) for i in range(5)]
+        dot_spec = '.'.join(path_elements)
+        result = self.loader._get_obj_value_from_dot_spec(obj, dot_spec)
+        expected_call_args_list = []
+        cursor = obj
+        for path_element in path_elements:
+            expected_call_args_list.append(call(obj=cursor, key=path_element))
+            next_cursor = self.loader._get_attr_or_item.return_value
+            cursor = next_cursor
+        self.assertEqual(self.loader._get_attr_or_item.call_args_list,
+                         expected_call_args_list)
+        self.assertEqual(result, self.loader._get_attr_or_item.return_value)
+
+class _GetAttrOrItemTestCase(BaseTestCase):
+    def _get(self, obj, key):
+        return self.loader._get_attr_or_item(obj=obj, key=key)
+
+    def test_gets_from_sequence(self):
+        seq = [MagicMock() for i in range(3)]
+        key = 0
+        self.assertEqual(self._get(seq, key),  seq[key])
+
+    def test_gets_from_mapping(self):
+        mapping = {'key_%s' % i: MagicMock() for i in range(3)}
+        key = list(mapping.keys())[1]
+        self.assertEqual(self._get(mapping, key),  mapping[key])
+
+    def test_gets_from_callable(self):
+        mapping = {'key_%s' % i: MagicMock() for i in range(3)}
+        items = list(mapping.items())
+        key = 1
+        self.assertEqual(self._get(mapping.items, key),  items[key])
+
+    def test_gets_from_attr(self):
+        obj = types.SimpleNamespace()
+        attrs = ['attr_%s' % i for i in range(3)]
+        for attr in attrs: setattr(obj, attr, MagicMock())
+        key = attrs[1]
+        self.assertEqual(self._get(obj, key),  getattr(obj, key))
