ce49c7aa3758b6e146282d1c832599f469f2b928
adorsk <adorsk@fas.harvard.edu>
2017-07-11 13:04:10 -0400

    fleshing out utils + tests

--- mc/houston/subcommands/_utils.py
+++ mc/houston/subcommands/_utils.py
@@ -1,11 +1,10 @@
 from jobman.jobman import JobMan
 
-from mc.artifact_processors.local_path_artifact_processor import (
-    LocalPathArtifactProcessor)
 from mc.clients.job_record_client import JobRecordClient
 from mc.clients.flow_record_client import FlowRecordClient
 from mc.daos.sqlalchemy_dao import SqlAlchemyDao as _McSqlAlchemyDao
-from mc.job_module_utils.dispatcher import JobModuleCommandDispatcher
+from mc.job_module_utils.job_module_command_dispatcher import (
+    JobModuleCommandDispatcher)
 from mc.runners.flow_runner import FlowRunner
 from mc.runners.jobman_job_runner.job_runner import JobRunner
 from mc.utils import import_utils
@@ -17,6 +16,13 @@ class HoustonSubcommandUtils(object):
 
         self._cfg = ...
         self._mc_dao = ...
+        self._flow_runner = ...
+        self._flow_record_client = ...
+        self._job_record_client = ...
+        self._flow_queue = ...
+        self._job_queue = ...
+        self._job_runner = ...
+        self._jobman = ...
 
     @property
     def cfg(self):
@@ -33,84 +39,73 @@ class HoustonSubcommandUtils(object):
     @mc_dao.setter
     def mc_dao(self, new_value): self._mc_dao = new_value
 
-    def get_flow_runner(self, mc_clients=None):
-        mc_clients = mc_clients or self.get_mc_clients()
-        task_ctx = {
-            'mc.flow_record_client': mc_clients['flow'],
-            'mc.job_record_client': mc_clients['job'],
-        }
-        return FlowRunner(flow_record_client=mc_clients['flow'],
-                          task_ctx=task_ctx)
+    @property
+    def flow_runner(self):
+        if self._flow_runner is ...: 
+            self._flow_runner = FlowRunner(
+                flow_record_client=self.flow_record_client,
+                task_ctx={
+                    'mc.flow_record_client': self.flow_record_client,
+                    'mc.job_record_client': self.job_record_client,
+                }
+            )
+        return self._flow_runner
 
-    def get_mc_clients(self, mc_dao=None):
-        mc_dao = mc_dao or self.mc_dao
-        return {
-            'flow': self.get_flow_record_client(mc_dao=mc_dao),
-            'job': self.get_job_record_client(mc_dao=mc_dao)
-        }
+    @flow_runner.setter
+    def flow_runner(self, new_value): self._flow_runner = new_value
 
-    def get_flow_record_client(self, mc_dao=None, queue_key=...):
-        mc_dao = mc_dao or self.mc_dao
-        if queue_key is ...: queue_key = self.get_mc_queues['flow']['key']
-        return FlowRecordClient(mc_dao=mc_dao, use_locks=True,
-                                queue_key=queue_key)
+    @property
+    def flow_record_client(self):
+        if self._flow_record_client is ...:
+            self._flow_record_client = FlowRecordClient(
+                mc_dao=self.mc_dao,
+                use_locks=self.cfg.get('USE_LOCKS', True),
+                queue_key=self.cfg['FLOW_QUEUE_KEY']
+            )
+        return self._flow_record_client
 
-    def get_mc_queues(self): raise NotImplementedError
+    @flow_record_client.setter
+    def flow_record_client(self, new_value):
+        self._flow_record_client = new_value
 
-    def _get_job_record_client(self, mc_dao=None, queue_key=...):
-        mc_dao = mc_dao or self.mc_dao
-        if queue_key is ...: queue_key = self.get_mc_queues['job']['key']
-        return JobRecordClient(mc_dao=mc_dao, use_locks=True,
-                               queue_key=queue_key)
+    @property
+    def job_record_client(self):
+        if self._job_record_client is ...:
+            self._job_record_client = JobRecordClient(
+                mc_dao=self.mc_dao,
+                use_locks=self.cfg.get('USE_LOCKS', True),
+                queue_key=self.cfg['JOB_QUEUE_KEY']
+            )
+        return self._job_record_client
 
-    def _get_job_runner(self, mc_clients=None):
-        return JobRunner(
-            artifact_processor=self._get_artifact_processor(),
-            job_record_client=mc_clients['job'],
-            jobman=self._get_jobman(),
-            submissions_dir=self.cfg['SUBMISSIONS_DIR'],
-            submission_factory=self._get_submission_factory(),
-        )
+    @job_record_client.setter
+    def job_record_client(self, new_value): self._job_record_client = new_value
 
-    def _get_artifact_processor(self): return LocalPathArtifactProcessor()
+    @property
+    def job_runner(self, mc_clients=None):
+        if self._job_runner is ...:
+            self._job_runner = JobRunner(
+                artifact_handler=self.cfg['ARTIFACT_HANDLER'],
+                job_record_client=self.job_record_client,
+                jobman=self.jobman,
+                jobdirs_dir=self.cfg.get('JOBDIRS_DIR', None),
+                build_jobdir_fn=self.build_jobdir,
+            )
+        return self._job_runner
 
-    def _get_jobman(self):
-        jobman_cfg = import_utils.load_module_from_path(
-            path=self.cfg['JOBMAN_CFG_PATH'])
-        return JobMan.from_cfg(cfg=jobman_cfg)
+    @job_runner.setter
+    def job_runner(self, new_value): self._job_runner = new_value
 
-    def _get_submission_factory(self):
-        class MySubmissionFactory(object):
-            def __init__(self_, cfg=None):
-                self_.cfg = cfg
+    @property
+    def jobman(self):
+        if self._jobman is ...:
+            jobman_cfg = import_utils.load_module_from_path(
+                path=self.cfg['JOBMAN_CFG_PATH'])
+            self._jobman = JobMan.from_cfg(cfg=jobman_cfg)
+        return self._jobman
 
-            def build_job_submission(self_, job=None, output_dir=None):
-                return JobModuleCommandDispatcher().build_job_submission(
-                    job=job, cfg=self_.cfg, output_dir=output_dir)
-        
-        return MySubmissionFactory(cfg={
-            'JOB_SUBMISSION_RUNNER_EXE': (
-                self.cfg['JOB_SUBMISSION_RUNNER_EXE']),
-            'SUBMISSION_BUILD_TARGET': 'bash'
-        })
+    @jobman.setter
+    def jobman(self, new_value): self._jobman = new_value
 
-    def _has_unfinished_items(self, mc_dao=None):
-        for item_type in ['Flow', 'Job']:
-            unfinished_items = self._get_unfinished_items(mc_dao=mc_dao,
-                                                          item_type=item_type)
-            if unfinished_items: return True
-        return False
-
-    def _get_unfinished_items(self, mc_dao=None, item_type=None):
-        unfinished_filter = {'prop': 'status', 'op': '! IN',
-                             'arg': ['COMPLETED', 'FAILED']}
-        return mc_dao.get_items(item_type=item_type,
-                                query={'filters':  [unfinished_filter]})
-
-    def _get_failed_flows(self, mc_dao=None):
-        return self._get_failed_items(mc_dao=mc_dao, item_type='Flow')
-
-    def _get_failed_items(self, mc_dao=None, item_type=None):
-        failed_filter = {'prop': 'status', 'op': '=', 'arg': 'FAILED'}
-        return mc_dao.get_items(item_type=item_type,
-                                query={'filters':  [failed_filter]})
+    def build_jobdir(self, *args, **kwargs):
+        return JobModuleCommandDispatcher().build_jobdir(*args, **kwargs)
--- mc/houston/subcommands/tests/test_utils.py
+++ mc/houston/subcommands/tests/test_utils.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest import call, MagicMock
+from unittest.mock import call, MagicMock, patch
 
 from .. import _utils
 
@@ -7,34 +7,181 @@ from .. import _utils
 class BaseTestCase(unittest.TestCase):
     def setUp(self):
         self.cfg = MagicMock()
-        self.utils = _utils.HoustonSubcomandUtils(
+        self.utils = _utils.HoustonSubcommandUtils(
             get_cfg=MagicMock(return_value=self.cfg)
         )
 
     def mockify_utils_attrs(self, attrs=None):
         for attr in attrs: setattr(self.utils, attr, MagicMock())
 
-class _GetFlowRunnerTestCase(BaseTestCase):
+    def mockify_module_attrs(self, attrs=None, module=_utils):
+        mod_mocks = {}
+        for attr in attrs:
+            patcher = patch.object(module, attr)
+            self.addCleanup(patcher.stop)
+            mod_mocks[attr] = patcher.start()
+        return mod_mocks
+
+class McDaoTestCase(BaseTestCase):
     def setUp(self):
         super().setUp()
-        self.mockify_subcommand_attrs(attrs=['_get_mc_client',
-                                             '_get_mc_queues'])
-        self.mod_mocks = self.mockify_module_attrs(attrs=['FlowRunner'])
-        self.result = self.subcommand._get_flow_runner()
+        self.mod_mocks = self.mockify_module_attrs(attrs=['_McSqlAlchemyDao'])
+        self.result = self.utils.mc_dao
 
-    def test_gets_mc_client(self):
-        self.assertEqual(self.subcommand._get_mc_client.call_args, call())
-        
-    def test_gets_flow_queue_key(self):
-        self.assertEqual(self.subcommand._get_mc_queues.call_args, call())
+    def test_constructs_dao(self):
+        self.assertEqual(self.mod_mocks['_McSqlAlchemyDao'].call_args,
+                         call(db_uri=self.cfg['MC_DB_URI']))
+
+    def test_ensures_tables(self):
+        self.assertEqual(
+            (self.mod_mocks['_McSqlAlchemyDao'].return_value.ensure_tables
+             .call_args),
+            call()
+        )
+
+    def test_returns_dao(self):
+        self.assertEqual(self.result,
+                         self.mod_mocks['_McSqlAlchemyDao'].return_value)
+
+    def test_memoizes(self):
+        self.assertTrue(self.utils.mc_dao is self.result)
+
+class FlowRunnerTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.mockify_utils_attrs(attrs=['flow_record_client',
+                                        'job_record_client'])
+        self.mod_mocks = self.mockify_module_attrs(attrs=['FlowRunner'])
+        self.result = self.utils.flow_runner
 
     def test_constructs_and_returns_flow_runner(self):
-        self.assertEqual(self.mod_mocks['FlowRunner'].call_args,
-                         call())
+        self.assertEqual(
+            self.mod_mocks['FlowRunner'].call_args,
+            call(
+                flow_record_client=self.utils.flow_record_client,
+                task_ctx={
+                    'mc.flow_record_client': self.utils.flow_record_client,
+                    'mc.job_record_client': self.utils.job_record_client,
+                }
+            )
+        )
         self.assertEqual(self.result, self.mod_mocks['FlowRunner'].return_value)
 
-class _GetMcClientTestCase(BaseTestCase):
-    def test_something(self): self.fail()
+    def test_memoizes(self):
+        self.assertTrue(self.utils.flow_runner is self.result)
 
-class _GetMcQueuesTestCase(BaseTestCase):
-    def test_something(self): self.fail()
+class FlowRecordClientTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.mockify_utils_attrs(attrs=['mc_dao'])
+        self.mod_mocks = self.mockify_module_attrs(attrs=['FlowRecordClient'])
+        self.result = self.utils.flow_record_client
+
+    def test_constructs_and_returns_flow_record_client(self):
+        self.assertEqual(
+            self.mod_mocks['FlowRecordClient'].call_args,
+            call(
+                mc_dao=self.utils.mc_dao,
+                use_locks=self.utils.cfg.get('USE_LOCKS', True),
+                queue_key=self.utils.cfg['FLOW_QUEUE_KEY']
+            )
+        )
+        self.assertEqual(self.result,
+                         self.mod_mocks['FlowRecordClient'].return_value)
+
+    def test_memoizes(self):
+        self.assertTrue(self.utils.flow_record_client is self.result)
+
+class JobRecordClientTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.mockify_utils_attrs(attrs=['mc_dao'])
+        self.mod_mocks = self.mockify_module_attrs(attrs=['JobRecordClient'])
+        self.result = self.utils.job_record_client
+
+    def test_constructs_and_returns_job_record_client(self):
+        self.assertEqual(
+            self.mod_mocks['JobRecordClient'].call_args,
+            call(
+                mc_dao=self.utils.mc_dao,
+                use_locks=self.utils.cfg.get('USE_LOCKS', True),
+                queue_key=self.utils.cfg['JOB_QUEUE_KEY']
+            )
+        )
+        self.assertEqual(self.result,
+                         self.mod_mocks['JobRecordClient'].return_value)
+
+    def test_memoizes(self):
+        self.assertTrue(self.utils.job_record_client is self.result)
+
+class JobRunnerTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.mockify_utils_attrs(attrs=['job_record_client', 'jobman',
+                                        'jobdir_factory'])
+        self.mod_mocks = self.mockify_module_attrs(attrs=['JobRunner'])
+        self.result = self.utils.job_runner
+
+    def test_constructs_and_returns_job_runner(self):
+        self.assertEqual(
+            self.mod_mocks['JobRunner'].call_args,
+            call(
+                artifact_handler=self.utils.cfg['ARTIFACT_HANDLER'],
+                job_record_client=self.utils.job_record_client,
+                jobman=self.utils.jobman,
+                jobdirs_dir=self.utils.cfg.get('JOBDIRS_DIR', None),
+                build_jobdir_fn=self.utils.build_jobdir
+            )
+        )
+        self.assertEqual(self.result, self.mod_mocks['JobRunner'].return_value)
+
+    def test_memoizes(self):
+        self.assertTrue(self.utils.job_runner is self.result)
+
+class JobManTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.mod_mocks = self.mockify_module_attrs(attrs=['JobMan',
+                                                          'import_utils'])
+        self.result = self.utils.jobman
+
+    def test_loads_jobman_cfg(self):
+        self.assertEqual(
+            self.mod_mocks['import_utils'].load_module_from_path.call_args,
+            call(path=self.utils.cfg['JOBMAN_CFG_PATH'])
+        )
+
+    def test_constructs_and_returns_jobman(self):
+        self.assertEqual(
+            self.mod_mocks['JobMan'].from_cfg.call_args,
+            call(cfg=(self.mod_mocks['import_utils'].load_module_from_path
+                      .return_value))
+        )
+        self.assertEqual(self.result,
+                         self.mod_mocks['JobMan'].from_cfg.return_value)
+
+    def test_memoizes(self):
+        self.assertTrue(self.utils.jobman is self.result)
+
+class BuildJobdirTestCase(BaseTestCase):
+    def setUp(self):
+        super().setUp()
+        self.mod_mocks = self.mockify_module_attrs(attrs=[
+            'JobModuleCommandDispatcher'])
+        self.args = [MagicMock() for i in range(3)]
+        self.kwargs = {'kwarg_%s' % i: MagicMock() for i in range(3)}
+        self.result = self.utils.build_jobdir(*self.args, **self.kwargs)
+
+    def test_dispatches_to_job_module_command_dispatcher(self):
+        self.assertEqual(self.mod_mocks['JobModuleCommandDispatcher'].call_args,
+                         call())
+        self.assertEqual(
+            (self.mod_mocks['JobModuleCommandDispatcher'].return_value
+             .build_jobdir.call_args),
+            call(*self.args, **self.kwargs)
+        )
+        self.assertEqual(
+            self.result, 
+            (self.mod_mocks['JobModuleCommandDispatcher'].return_value
+             .build_jobdir.return_value)
+        )
